2f9065705f8c74a915f061f6f0bf487c70a8cab1
Dave Moten <davidmoten@gmail.com>
2014-12-03 10:17:46 +1100

    update release.sh

--- release.sh
+++ release.sh
@@ -9,4 +9,4 @@ DRY_RUN=false
 git pull
 mvn release:clean
 mvn -B release:prepare -DreleaseVersion=$VERSION -DautoVersionSubmodules=true -Dtag=$VERSION -DdryRun=$DRY_RUN
-mvn release:perform -Darguments='-Drisky.repo=amsa-maven -Dmode=dev -Drisky.repo.url=http://sardevc.amsa.gov.au:8081/artifactory/libs-snapshots-local' -DdryRun=$DRY_RUN
+mvn release:perform -Darguments='-Drisky.repo=amsa-maven -Dmode=test -Drisky.repo.url=http://sardevc.amsa.gov.au:8081/artifactory/libs-releases-local' -DdryRun=$DRY_RUN
