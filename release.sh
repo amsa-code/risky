483631504a8f01444fe2c38b771f0b1ec4c83c7d
Dave Moten <davidmoten@gmail.com>
2014-12-03 13:25:11 +1100

    update release.sh

--- release.sh
+++ release.sh
@@ -5,6 +5,20 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 VERSION=$1
+git checkout master
+git pull
+mvn versions:set -DnewVersion=$VERSION
+mvn clean install 
+git commit -am "prepare for release $VERSION"
+git push
+git tag -a $VERSION -m "$VERSION"
+git push origin $VERSION
+git checkout $VERSION
+mvn deploy -Dmode=test -Drisky.repo=amsa-maven -Drisky.repo.url=http://sardevc.amsa.gov.au:8081/artifactory/libs-releases-local
+git checkout master
+mvn versions:set -DnewVersion=$VERSION.1-SNAPSHOT
+git commit -am "setting versions to snapshot"
+git push
 
 ## could not get release plugin to work for git push!
 #GIT_USERNAME=$2
