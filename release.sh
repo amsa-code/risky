81fba111ceb94b052e44e101ae2921c692c7582d
Dave Moten <davidmoten@gmail.com>
2014-12-03 12:35:20 +1100

    update script

--- release.sh
+++ release.sh
@@ -10,5 +10,5 @@ GIT_PASSWORD=$3
 DRY_RUN=false
 git pull
 mvn release:clean
-mvn -B release:prepare -DreleaseVersion=$VERSION -DautoVersionSubmodules=true -Dtag=$VERSION -DdryRun=$DRY_RUN -Dusername=$GIT_USERNAME -Dpassword=$GIT_PASSWORD
+mvn -B -X release:prepare -DreleaseVersion=$VERSION -DautoVersionSubmodules=true -Dtag=$VERSION -DdryRun=$DRY_RUN -Dusername=$GIT_USERNAME -Dpassword=$GIT_PASSWORD
 mvn release:perform -X -Darguments='-Drisky.repo=amsa-maven -Dmode=test -Drisky.repo.url=http://sardevc.amsa.gov.au:8081/artifactory/libs-releases-local' -DdryRun=$DRY_RUN -Dusername=$GIT_USERNAME -Dpassword=$GIT_PASSWORD
