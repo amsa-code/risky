ce08af2a353ddbb533489eae2abbce23b0927ba9
Dave Moten <davidmoten@gmail.com>
2014-12-03 13:18:59 +1100

    fix poms

--- release.sh
+++ release.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 if [ $# -eq 0 ]; then
-    echo "Usage: ./release.sh <VERSION> <GIT_USERNAME> <GIT_PASSWORD>"
+    echo "Usage: ./release.sh <VERSION>"
     exit 1
 fi
 VERSION=$1
-GIT_USERNAME=$2
-GIT_PASSWORD=$3
-DRY_RUN=false
-git pull
-mvn release:clean
-mvn -B -X release:prepare -DreleaseVersion=$VERSION -DautoVersionSubmodules=true -Dtag=$VERSION -DdryRun=$DRY_RUN -Dusername=$GIT_USERNAME -Dpassword=$GIT_PASSWORD
-mvn release:perform -X -Darguments='-Drisky.repo=amsa-maven -Dmode=test -Drisky.repo.url=http://sardevc.amsa.gov.au:8081/artifactory/libs-releases-local' -DdryRun=$DRY_RUN -Dusername=$GIT_USERNAME -Dpassword=$GIT_PASSWORD
+
+## could not get release plugin to work for git push!
+#GIT_USERNAME=$2
+#GIT_PASSWORD=$3
+#DRY_RUN=false
+#git pull
+#mvn release:clean
+#mvn -B -X release:prepare -DreleaseVersion=$VERSION -DautoVersionSubmodules=true -Dtag=$VERSION -DdryRun=#$DRY_RUN -Dusername=$GIT_USERNAME -Dpassword=$GIT_PASSWORD
+#mvn release:perform -X -Darguments='-Drisky.repo=amsa-maven -Dmode=test -Drisky.repo.url=http://sardevc.amsa.gov.au:8081/artifactory/libs-releases-local' -DdryRun=$DRY_RUN -Dusername=$GIT_USERNAME -#Dpassword=$GIT_PASSWORD
