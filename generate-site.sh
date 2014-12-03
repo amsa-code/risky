ba6a9f2739bb877207695aaa0948492d568c614c
Dave Moten <davidmoten@gmail.com>
2014-12-03 15:43:24 +1100

    add site reports to pom and script to generate in gh-pages

---
+++ generate-site.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+PROJECT=risky
+mvn clean install 
+mvn site
+mvn site:stage -Dsite.url=file://`pwd`/target/staging-temp
+cd ../amsa-code.github.io
+git pull
+mkdir -p $PROJECT
+cp -r ../$PROJECT/target/staging/* $PROJECT/
+git add .
+git commit -am "update site reports for $PROJECT"
+git push
+
