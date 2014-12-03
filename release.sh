90ed36fbe3182b9c5fda370f543bd4793bf0388e
Dave Moten <davidmoten@gmail.com>
2014-12-03 12:06:04 +1100

    update poms

--- release.sh
+++ release.sh
@@ -11,4 +11,4 @@ DRY_RUN=false
 git pull
 mvn release:clean
 mvn -B release:prepare -DreleaseVersion=$VERSION -DautoVersionSubmodules=true -Dtag=$VERSION -DdryRun=$DRY_RUN
-mvn release:perform -Darguments='-Drisky.repo=amsa-maven -Dmode=test -Drisky.repo.url=http://sardevc.amsa.gov.au:8081/artifactory/libs-releases-local' -DdryRun=$DRY_RUN -Dusername=$GIT_USERNAME -Dpassword=$GIT_PASSWORD
+mvn release:perform -X -Darguments='-Drisky.repo=amsa-maven -Dmode=test -Drisky.repo.url=http://sardevc.amsa.gov.au:8081/artifactory/libs-releases-local' -DdryRun=$DRY_RUN -Dusername=$GIT_USERNAME -Dpassword=$GIT_PASSWORD
