ea8c5c173732d912a1956368c515287eb41b6c6b
Dave Moten <davidmoten@gmail.com>
2014-12-03 12:00:49 +1100

    update release.sh

--- release.sh
+++ release.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 if [ $# -eq 0 ]; then
-    echo "Usage: ./release.sh <VERSION>"
+    echo "Usage: ./release.sh <VERSION> <GIT_USERNAME> <GIT_PASSWORD>"
     exit 1
 fi
 VERSION=$1
+GIT_USERNAME=$2
+GIT_PASSWORD=$3
 DRY_RUN=false
 git pull
 mvn release:clean
 mvn -B release:prepare -DreleaseVersion=$VERSION -DautoVersionSubmodules=true -Dtag=$VERSION -DdryRun=$DRY_RUN
-mvn release:perform -Darguments='-Drisky.repo=amsa-maven -Dmode=test -Drisky.repo.url=http://sardevc.amsa.gov.au:8081/artifactory/libs-releases-local' -DdryRun=$DRY_RUN
+mvn release:perform -Darguments='-Drisky.repo=amsa-maven -Dmode=test -Drisky.repo.url=http://sardevc.amsa.gov.au:8081/artifactory/libs-releases-local' -DdryRun=$DRY_RUN -Dusername=$GIT_USERNAME -Dpassword=$GIT_PASSWORD
