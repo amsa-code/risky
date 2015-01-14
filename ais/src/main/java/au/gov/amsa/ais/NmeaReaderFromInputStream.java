e0aa71da73b5b05723d565c849901749a1b0dde5
Dave Moten <davidmoten@gmail.com>
2015-01-14 15:29:24 +1100

    ignore whitespace lines

--- ais/src/main/java/au/gov/amsa/ais/NmeaReaderFromInputStream.java
+++ ais/src/main/java/au/gov/amsa/ais/NmeaReaderFromInputStream.java
@@ -4,11 +4,19 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
+import rx.functions.Func1;
 import rx.observables.StringObservable;
+import au.gov.amsa.streams.Strings;
 import au.gov.amsa.util.nmea.NmeaReader;
 
 public class NmeaReaderFromInputStream implements NmeaReader {
 
+	private static final Func1<String, Boolean> NON_EMPTY = new Func1<String,Boolean>(){
+		@Override
+		public Boolean call(String s) {
+			return s.trim().length()>0;
+		}};
+		
 	private final InputStream is;
 
 	public NmeaReaderFromInputStream(InputStream is) {
@@ -17,8 +25,8 @@ public class NmeaReaderFromInputStream implements NmeaReader {
 
 	@Override
 	public Iterable<String> read() {
-		return StringObservable
-				.split(StringObservable.from(new InputStreamReader(is, Charset
-						.forName("UTF-8"))), "\n").toBlocking().toIterable();
+		return Strings
+				.split(Strings.from(new InputStreamReader(is, Charset
+						.forName("UTF-8"))), "\n").filter(NON_EMPTY).toBlocking().toIterable();
 	}
 }
