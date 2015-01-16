f3be985857b1a46ff1afe0b2f66edce122211f9e
Dave Moten <davidmoten@gmail.com>
2015-01-16 16:01:08 +1100

    write just two fixes

--- formats/src/test/java/au/gov/amsa/risky/format/BinaryFixesTest.java
+++ formats/src/test/java/au/gov/amsa/risky/format/BinaryFixesTest.java
@@ -54,10 +54,10 @@ public final class BinaryFixesTest {
 	@Test
 	public void testWriteTwoBinaryFixes() throws IOException {
 		OutputStream os = new BufferedOutputStream(new FileOutputStream("target/123456790.track"));
-		Fix fix1 = new Fix(213456789, -10f, 135f, 1000,
+		Fix fix1 = new Fix(213456789, -10f, 135f, 0,
 				of(NavigationalStatus.ENGAGED_IN_FISHING), of(7.5f), of(45f),
 				of(46f), AisClass.B);
-		Fix fix2 = new Fix(213456789, -10.1f, 135.2f, 1000,
+		Fix fix2 = new Fix(213456789, -10.1f, 135.2f, 1000*3600*2L,
 				of(NavigationalStatus.AT_ANCHOR), of(4.5f), of(20f),
 				of(30f), AisClass.B);
 		BinaryFixes.write(fix1, os);
