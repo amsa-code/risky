c33cd86a9ef8a5539da0038fc074ee22a5c8b04e
Dave Moten <davidmoten@gmail.com>
2015-02-25 09:42:58 +1100

    load binary fixes dev

--- formats/src/test/java/au/gov/amsa/risky/format/BinaryFixesMain.java
+++ formats/src/test/java/au/gov/amsa/risky/format/BinaryFixesMain.java
@@ -1,14 +1,17 @@
 package au.gov.amsa.risky.format;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Pattern;
 
 import rx.Observable;
+import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.functions.Func2;
 import rx.schedulers.Schedulers;
-import au.gov.amsa.risky.format.BinaryFixes;
 import au.gov.amsa.util.Files;
 
 import com.github.davidmoten.rx.slf4j.Logging;
@@ -17,6 +20,8 @@ public class BinaryFixesMain {
 
 	public static void main(String[] args) {
 		//perform a speed test for loading BinaryFixes from disk
+		
+		final ConcurrentHashMap<Long, List<Fix>> map = new ConcurrentHashMap<Long, List<Fix>>();
 		
 		List<File> files = Files.find(new File(
 				"/media/an/binary-fixes/2014-year-downsample-5-mins"), Pattern
@@ -29,9 +34,14 @@ public class BinaryFixesMain {
 				.flatMap(new Func1<File, Observable<Long>>() {
 					@Override
 					public Observable<Long> call(File file) {
-						return BinaryFixes.from(file).countLong()
+						return BinaryFixes.from(file)
+								//add to map
+								//.doOnNext(addToMap(map))
+								//count
+								.countLong()
 								.subscribeOn(Schedulers.computation());
 					}
+					
 				})
 				// total counts
 				.scan(0L, new Func2<Long, Long, Long>() {
@@ -48,7 +58,21 @@ public class BinaryFixesMain {
 				//block and get
 				.toBlocking().single();
 		long elapsed = System.currentTimeMillis() - t;
+		System.out.println("Map size = " + map.size());
 		System.out.println("Total records = " + count + ", numPerSecond="
 				+ count * 1000.0 / elapsed + ", timeMs=" + elapsed);
+	}
+	
+	private static Action1<? super Fix> addToMap(final ConcurrentHashMap<Long, List<Fix>> map){
+		return new Action1<Fix>() {
+			@Override
+			public void call(Fix fix) {
+					List<Fix> list = map.get(fix.getMmsi());
+					if (list==null) {
+						map.putIfAbsent(fix.getMmsi(), Collections.synchronizedList( new ArrayList<Fix>()));
+						list = map.get(fix.getMmsi());
+					}
+					list.add(fix);
+			}};
 	}
 }
