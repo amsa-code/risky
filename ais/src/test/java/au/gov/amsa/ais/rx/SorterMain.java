ddc83e856baa05c7b06bbe273cde224a0adf0a3d
Dave Moten <davidmoten@gmail.com>
2015-03-05 07:32:21 +1100

    make SorterMain accessible via pom

--- ais/src/test/java/au/gov/amsa/ais/rx/SorterMain.java
+++ ais/src/test/java/au/gov/amsa/ais/rx/SorterMain.java
@@ -6,12 +6,12 @@ import rx.schedulers.Schedulers;
 
 public class SorterMain {
 
-	public static void main(String[] args) throws InterruptedException {
-		String output = System.getProperty("output", "target/output");
-		output = "/media/an/binary-fixes-2012/temp";
-		long sampleSeconds = Long.parseLong(System.getProperty("sampleSeconds", "0"));
-		Streams.sortBinaryFixFilesByTime(new File(output), sampleSeconds, Schedulers.immediate())
-		        .count().toBlocking().single();
-	}
+    public static void main(String[] args) throws InterruptedException {
+        String output = System.getProperty("output", "target/output");
+        // output = "/media/an/binary-fixes-2012/temp";
+        long sampleSeconds = Long.parseLong(System.getProperty("sampleSeconds", "0"));
+        Streams.sortBinaryFixFilesByTime(new File(output), sampleSeconds, Schedulers.immediate())
+                .count().toBlocking().single();
+    }
 
 }
