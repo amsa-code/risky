c9b78d300b43483f02f630d03893f21e69b64ae7
Dave Moten <davidmoten@gmail.com>
2015-03-27 11:47:11 +1100

    update spark, use distinctUntilChanged in downsample so don't run out of memory

--- formats/src/main/java/au/gov/amsa/risky/format/Downsample.java
+++ formats/src/main/java/au/gov/amsa/risky/format/Downsample.java
@@ -18,54 +18,61 @@ import com.github.davidmoten.rx.Functions;
  */
 public class Downsample<T extends HasFix> implements Transformer<T, T> {
 
-	private long maxTimeBetweenFixesMs;
-	private Func1<T, Boolean> selector;
+    private long minTimeBetweenFixesMs;
+    private Func1<T, Boolean> selector;
 
-	public Downsample(long minTimeBetweenFixesMs, Func1<T, Boolean> selector) {
-		this.maxTimeBetweenFixesMs = minTimeBetweenFixesMs;
-		this.selector = selector;
-	}
+    public Downsample(long minTimeBetweenFixesMs, Func1<T, Boolean> selector) {
+        this.minTimeBetweenFixesMs = minTimeBetweenFixesMs;
+        this.selector = selector;
+    }
 
-	public static <T extends HasFix> Downsample<T> minTimeStep(long duration, TimeUnit unit) {
-		return new Downsample<T>(unit.toMillis(duration), Functions.<T> alwaysFalse());
-	}
+    public static <T extends HasFix> Downsample<T> minTimeStep(long duration, TimeUnit unit) {
+        return new Downsample<T>(unit.toMillis(duration), Functions.<T> alwaysFalse());
+    }
 
-	public static <T extends HasFix> Downsample<T> minTimeStep(long duration, TimeUnit unit,
-	        Func1<T, Boolean> selector) {
-		return new Downsample<T>(unit.toMillis(duration), selector);
-	}
+    public static <T extends HasFix> Downsample<T> minTimeStep(long duration, TimeUnit unit,
+            Func1<T, Boolean> selector) {
+        return new Downsample<T>(unit.toMillis(duration), selector);
+    }
 
-	@Override
-	public Observable<T> call(Observable<T> fixes) {
-		return fixes.scan(new Func2<T, T, T>() {
-			@Override
-			public T call(T latest, T fix) {
-				if (fix.fix().getTime() < latest.fix().getTime())
-					throw new RuntimeException("not in ascending time order!");
-				else if (fix.fix().getMmsi() != latest.fix().getMmsi())
-					throw new RuntimeException("can only downsample a single vessel");
-				else if (fix.fix().getTime() - latest.fix().getTime() >= maxTimeBetweenFixesMs
-				        || selector.call(fix))
-					return fix;
-				else
-					return latest;
-			}
-		})
-		// throw away repeats
-		        .distinct();
-	}
+    @Override
+    public Observable<T> call(Observable<T> fixes) {
+        Observable<T> result = fixes.scan(new Func2<T, T, T>() {
+            @Override
+            public T call(T latest, T fix) {
+                if (fix.fix().getTime() < latest.fix().getTime())
+                    throw new RuntimeException("not in ascending time order!");
+                else if (fix.fix().getMmsi() != latest.fix().getMmsi())
+                    throw new RuntimeException("can only downsample a single vessel");
+                else if (fix.fix().getTime() - latest.fix().getTime() >= minTimeBetweenFixesMs
+                        || selector.call(fix))
+                    return fix;
+                else
+                    return latest;
+            }
+        });
+        if (minTimeBetweenFixesMs > 0)
+            // throw away repeats
+            result = result.distinctUntilChanged(new Func1<HasFix, Long>() {
+                @Override
+                public Long call(HasFix f) {
+                    return f.fix().getTime();
+                }
+            });
+        return result;
+    }
 
-	public static Observable<Integer> downsample(final File input, final File output,
-	        Pattern pattern, final long duration, final TimeUnit unit) {
-		return Formats.transform(input, output, pattern, Downsample.minTimeStep(duration, unit),
-		        FIXES_WRITER, Functions.<String> identity());
-	}
+    public static Observable<Integer> downsample(final File input, final File output,
+            Pattern pattern, final long duration, final TimeUnit unit) {
+        return Formats.transform(input, output, pattern, Downsample.minTimeStep(duration, unit),
+                FIXES_WRITER, Functions.<String> identity());
+    }
 
-	private static Action2<List<HasFix>, File> FIXES_WRITER = new Action2<List<HasFix>, File>() {
-		@Override
-		public void call(List<HasFix> list, File file) {
-			BinaryFixesWriter.writeFixes(list, file, false, false);
-		}
-	};
+    private static Action2<List<HasFix>, File> FIXES_WRITER = new Action2<List<HasFix>, File>() {
+        @Override
+        public void call(List<HasFix> list, File file) {
+            BinaryFixesWriter.writeFixes(list, file, false, false);
+        }
+    };
 
 }
