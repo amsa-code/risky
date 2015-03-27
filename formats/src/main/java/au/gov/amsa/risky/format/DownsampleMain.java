3ec98d7f47e97038054a9b78d2a1f6fbbf6ac5c1
Dave Moten <davidmoten@gmail.com>
2015-03-27 13:56:38 +1100

    fix logging for sorting

--- formats/src/main/java/au/gov/amsa/risky/format/DownsampleMain.java
+++ formats/src/main/java/au/gov/amsa/risky/format/DownsampleMain.java
@@ -6,19 +6,18 @@ import java.util.regex.Pattern;
 
 public class DownsampleMain {
 
-	public static void main(String[] args) {
-		try {
-			File input = new File(System.getProperty("input"));
-			File output = new File(System.getProperty("output"));
-			Pattern pattern = Pattern.compile(System.getProperty("pattern"));
-			long intervalMs = Long.parseLong(System.getProperty("ms"));
-			Downsample
-					.downsample(input, output, pattern, intervalMs,
-							TimeUnit.MILLISECONDS).count().toBlocking()
-					.single();
-		} catch (RuntimeException e) {
-			System.out
-					.println("Usage: -Dinput=<input directory> -Doutput=<output directory> -Dpattern=<filename pattern> -Dms=<downsample interval ms>");
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            File input = new File(System.getProperty("input"));
+            File output = new File(System.getProperty("output"));
+            Pattern pattern = Pattern.compile(System.getProperty("pattern"));
+            long intervalMs = Long.parseLong(System.getProperty("ms"));
+            Downsample.downsample(input, output, pattern, intervalMs, TimeUnit.MILLISECONDS)
+                    .count().toBlocking().single();
+        } catch (RuntimeException e) {
+            System.out
+                    .println("Usage: -Dinput=<input directory> -Doutput=<output directory> -Dpattern=<filename pattern> -Dms=<downsample interval ms>");
+            throw e;
+        }
+    }
 }
--- formats/src/main/java/au/gov/amsa/risky/format/Formats.java
+++ formats/src/main/java/au/gov/amsa/risky/format/Formats.java
@@ -15,68 +15,65 @@ import rx.functions.Func1;
 import au.gov.amsa.util.Files;
 
 import com.github.davidmoten.rx.Functions;
-import com.github.davidmoten.rx.operators.OperatorUnsubscribeEagerly;
 import com.github.davidmoten.util.Preconditions;
 import com.google.common.annotations.VisibleForTesting;
 
 public final class Formats {
 
-	private static final Logger log = LoggerFactory.getLogger(Formats.class);
+    private static final Logger log = LoggerFactory.getLogger(Formats.class);
 
-	public static Observable<Integer> transform(final File input, final File output,
-	        Pattern pattern, final Transformer<HasFix, HasFix> transformer,
-	        final Action2<List<HasFix>, File> fixesWriter, final Func1<String, String> renamer) {
-		Preconditions.checkNotNull(input);
-		Preconditions.checkNotNull(output);
-		Preconditions.checkNotNull(pattern);
-		Preconditions.checkNotNull(transformer);
-		List<File> files = Files.find(input, pattern);
-		log.info("converting " + files.size() + " files");
-		return Observable
-		// get the files matching the pattern from the directory
-		        .from(files)
-		        // replace the file with a transformed version
-		        .flatMap(new Func1<File, Observable<Integer>>() {
+    public static Observable<Integer> transform(final File input, final File output,
+            Pattern pattern, final Transformer<HasFix, HasFix> transformer,
+            final Action2<List<HasFix>, File> fixesWriter, final Func1<String, String> renamer) {
+        Preconditions.checkNotNull(input);
+        Preconditions.checkNotNull(output);
+        Preconditions.checkNotNull(pattern);
+        Preconditions.checkNotNull(transformer);
+        List<File> files = Files.find(input, pattern);
+        log.info("converting " + files.size() + " files");
+        return Observable
+        // get the files matching the pattern from the directory
+                .from(files)
+                // replace the file with a transformed version
+                .flatMap(new Func1<File, Observable<Integer>>() {
 
-			        @Override
-			        public Observable<Integer> call(final File file) {
-				        final File outputFile = rebase(file, input, output);
-				        outputFile.getParentFile().mkdirs();
-				        return BinaryFixes.from(file)
-				        // ensure file is closed in case we want to
-				        // rewrite downstream
-				                .lift(OperatorUnsubscribeEagerly.<Fix> instance())
-				                // to list
-				                .toList()
-				                // flatten
-				                .flatMapIterable(Functions.<List<Fix>> identity())
-				                // transform the fixes
-				                .compose(transformer)
-				                // make into a list again
-				                .toList()
-				                // replace the file with sorted fixes
-				                .doOnNext(new Action1<List<HasFix>>() {
-					                @Override
-					                public void call(List<HasFix> list) {
-						                File f = new File(outputFile.getParentFile(), renamer
-						                        .call(outputFile.getName()));
-						                fixesWriter.call(list, f);
-						                log.info("transformed " + file + " to " + f);
-					                }
-				                })
-				                // count the fixes
-				                .count();
-			        }
-		        });
+                    @Override
+                    public Observable<Integer> call(final File file) {
+                        final File outputFile = rebase(file, input, output);
+                        outputFile.getParentFile().mkdirs();
+                        log.info("writing " + file + " to " + outputFile);
+                        return BinaryFixes.from(file, true)
+                        // to list
+                                .toList()
+                                // flatten
+                                .flatMapIterable(Functions.<List<Fix>> identity())
+                                // transform the fixes
+                                .compose(transformer)
+                                // make into a list again
+                                .toList()
+                                // replace the file with sorted fixes
+                                .doOnNext(new Action1<List<HasFix>>() {
+                                    @Override
+                                    public void call(List<HasFix> list) {
+                                        File f = new File(outputFile.getParentFile(), renamer
+                                                .call(outputFile.getName()));
+                                        fixesWriter.call(list, f);
+                                        log.info("transformed " + file + " to " + f);
+                                    }
+                                })
+                                // count the fixes
+                                .count();
+                    }
+                });
 
-	}
+    }
 
-	@VisibleForTesting
-	static File rebase(File file, File existingParent, File newParent) {
-		if (file.getAbsolutePath().equals(existingParent.getAbsolutePath()))
-			return newParent;
-		else
-			return new File(rebase(file.getParentFile(), existingParent, newParent), file.getName());
-	}
+    @VisibleForTesting
+    static File rebase(File file, File existingParent, File newParent) {
+        if (file.getAbsolutePath().equals(existingParent.getAbsolutePath()))
+            return newParent;
+        else
+            return new File(rebase(file.getParentFile(), existingParent, newParent), file.getName());
+    }
 
 }
