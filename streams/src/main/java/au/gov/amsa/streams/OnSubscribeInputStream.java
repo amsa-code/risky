5c6dfacf6ef2a9e220a417dabfded5fece48efb3
Dave Moten <davidmoten@gmail.com>
2015-04-10 17:01:26 +1000

    optimize OnSubscribeInputStream

--- streams/src/main/java/au/gov/amsa/streams/OnSubscribeInputStream.java
+++ streams/src/main/java/au/gov/amsa/streams/OnSubscribeInputStream.java
@@ -7,36 +7,37 @@ import java.util.Arrays;
 import rx.Subscriber;
 import rx.observables.AbstractOnSubscribe;
 
-public final class OnSubscribeInputStream extends
-		AbstractOnSubscribe<byte[], InputStream> {
+public final class OnSubscribeInputStream extends AbstractOnSubscribe<byte[], InputStream> {
 
-	private final InputStream is;
-	private final int size;
+    private final InputStream is;
+    private final int size;
 
-	public OnSubscribeInputStream(InputStream is, int size) {
-		this.is = is;
-		this.size = size;
-	}
+    public OnSubscribeInputStream(InputStream is, int size) {
+        this.is = is;
+        this.size = size;
+    }
 
-	@Override
-	protected InputStream onSubscribe(Subscriber<? super byte[]> subscriber) {
-		return is;
-	}
+    @Override
+    protected InputStream onSubscribe(Subscriber<? super byte[]> subscriber) {
+        return is;
+    }
 
-	@Override
-	protected void next(
-			rx.observables.AbstractOnSubscribe.SubscriptionState<byte[], InputStream> state) {
+    @Override
+    protected void next(
+            rx.observables.AbstractOnSubscribe.SubscriptionState<byte[], InputStream> state) {
 
-		InputStream is = state.state();
-		byte[] buffer = new byte[size];
-		try {
-			int count = is.read(buffer);
-			if (count == -1)
-				state.onCompleted();
-			else
-				state.onNext(Arrays.copyOf(buffer, count));
-		} catch (IOException e) {
-			state.onError(e);
-		}
-	}
+        InputStream is = state.state();
+        byte[] buffer = new byte[size];
+        try {
+            int count = is.read(buffer);
+            if (count == -1)
+                state.onCompleted();
+            else if (count < size)
+                state.onNext(Arrays.copyOf(buffer, count));
+            else
+                state.onNext(buffer);
+        } catch (IOException e) {
+            state.onError(e);
+        }
+    }
 }
