9548229b3951e1957b69ec30b76fd940365b3d23
Dave Moten <davidmoten@gmail.com>
2015-05-21 16:32:22 +1000

    drift detector dev

--- behaviour-detector/src/test/java/au/gov/amsa/navigation/DriftDetectorMain.java
+++ behaviour-detector/src/test/java/au/gov/amsa/navigation/DriftDetectorMain.java
@@ -6,20 +6,25 @@ import java.io.IOException;
 import au.gov.amsa.ais.rx.Streams;
 import au.gov.amsa.navigation.ais.AisVesselPositions;
 
+import com.github.davidmoten.rx.slf4j.Logging;
+
 public class DriftDetectorMain {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        String filename = "/media/analysis/nmea/2013/NMEA_ITU_20130108.gz";
-        Streams.nmeaFromGzip(filename).compose(AisVesselPositions.positions())
-                .compose(DriftDetector.detectDrift())
+        String filename = "/media/an/nmea/2013/NMEA_ITU_20130108.gz";
+        Streams.nmeaFromGzip(filename).lift(Logging.<String> logger().every(10000).log())
+        //
+                .compose(AisVesselPositions.positions()).compose(DriftDetector.detectDrift())
                 // group by mmsi
                 .groupBy(f -> f.fix().mmsi())
                 // first position
                 .flatMap(positions -> positions.first())
                 //
                 .doOnNext(System.out::println)
-                // go
-                .subscribe();
+                //
+                .doOnError(System.err::println)
+                //
+                .count().toBlocking().single();
     }
 
 }
