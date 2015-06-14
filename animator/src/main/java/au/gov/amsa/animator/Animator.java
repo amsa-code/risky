b182b3081a86d1e27757d21d67befa5668a89e41
Dave Moten <davidmoten@gmail.com>
2015-06-14 20:16:40 +1000

    animator dev

--- animator/src/main/java/au/gov/amsa/animator/Animator.java
+++ animator/src/main/java/au/gov/amsa/animator/Animator.java
@@ -47,141 +47,152 @@ import com.vividsolutions.jts.geom.Point;
 
 public class Animator {
 
-	private static final float CANBERRA_LAT = -35.3075f;
-	private static final float CANBERRA_LONG = 149.1244f;
-	volatile Timer timer;
-	final Worker worker = Schedulers.newThread().createWorker();
+    private static final float CANBERRA_LAT = -35.3075f;
+    private static final float CANBERRA_LONG = 149.1244f;
+    volatile Timer timer;
+    final Worker worker = Schedulers.newThread().createWorker();
 
-	public void start() {
-		// Create a map context and add our shapefile to it
-		final MapContent map = createMap();
+    public void start() {
+        // Create a map context and add our shapefile to it
+        final MapContent map = createMap();
 
-		// Now display the map using the custom renderer
-		display(map);
-	}
+        // Now display the map using the custom renderer
+        display(map);
 
-	private MapContent createMap() {
-		final MapContent map = new MapContent();
-		map.setTitle("Animator");
-		map.addLayer(createCoastlineLayer());
-		map.addLayer(createExtraFeatures());
-		// addWms(map);
-		return map;
-	}
+        System.exit(0);
+        // animate
+        long timeStep = 0;
+        long frameMs = 50;
+        while (true) {
+            long t = System.currentTimeMillis();
+            // mapPane.repaint();
+            // updateModel(timeStep);
+            // drawModel(offScreenImage);
+            timeStep++;
+            try {
+                Thread.sleep(Math.max(0, t + frameMs - System.currentTimeMillis()));
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+    }
 
-	private void display(final MapContent map) {
-		EventQueue.invokeLater(() -> {
-			// setup custom rendering over the top of the map
-				BackgroundRenderer renderer = new BackgroundRenderer();
-				JMapPane mapPane = new JMapPane(map) {
-					int n = 0;
+    private MapContent createMap() {
+        final MapContent map = new MapContent();
+        map.setTitle("Animator");
+        map.addLayer(createCoastlineLayer());
+        map.addLayer(createExtraFeatures());
+        // addWms(map);
+        return map;
+    }
 
-					@Override
-					protected void paintComponent(Graphics g) {
-						super.paintComponent(g);
-						if (renderer.worldToScreen() != null) {
-							Graphics2D g2 = (Graphics2D) g;
-							n++;
-							Point2D.Float p = new Point2D.Float(CANBERRA_LONG,
-									CANBERRA_LAT);
-							Point2D.Float q = new Point2D.Float();
-							renderer.worldToScreen().transform(p, q);
-							g2.drawString("Hello", q.x + n % 100, q.y + n % 100);
-						}
-					}
-				};
-				final JMapFrame frame = new JMapFrame(map, mapPane);
-				frame.getMapPane().setRenderer(renderer);
-				frame.enableStatusBar(true);
-				frame.enableToolBar(true);
-				frame.initComponents();
-				frame.setSize(800, 600);
-				FramePreferences.restoreLocationAndSize(frame, 100, 100, 800,
-						600, Animator.class);
-				frame.getMapPane().addMouseListener(new MapMouseAdapter() {
+    private void display(final MapContent map) {
+        EventQueue.invokeLater(() -> {
+            // setup custom rendering over the top of the map
+                BackgroundRenderer renderer = new BackgroundRenderer();
+                JMapPane mapPane = new JMapPane(map) {
+                    int n = 0;
 
-					@Override
-					public void onMouseClicked(MapMouseEvent event) {
-						DirectPosition2D p = event.getWorldPos();
-						System.out.println(p);
-					}
+                    @Override
+                    protected void paintComponent(Graphics g) {
+                        super.paintComponent(g);
+                        if (renderer.worldToScreen() != null) {
+                            Graphics2D g2 = (Graphics2D) g;
+                            n++;
+                            Point2D.Float p = new Point2D.Float(CANBERRA_LONG, CANBERRA_LAT);
+                            Point2D.Float q = new Point2D.Float();
+                            renderer.worldToScreen().transform(p, q);
+                            g2.drawString("Hello", q.x + n % 100, q.y + n % 100);
+                        }
+                    }
+                };
+                final JMapFrame frame = new JMapFrame(map, mapPane);
+                frame.getMapPane().setRenderer(renderer);
+                frame.enableStatusBar(true);
+                frame.enableToolBar(true);
+                frame.initComponents();
+                frame.setSize(800, 600);
+                FramePreferences.restoreLocationAndSize(frame, 100, 100, 800, 600, Animator.class);
+                frame.getMapPane().addMouseListener(new MapMouseAdapter() {
 
-				});
+                    @Override
+                    public void onMouseClicked(MapMouseEvent event) {
+                        DirectPosition2D p = event.getWorldPos();
+                        System.out.println(p);
+                    }
 
-				frame.setVisible(true);
-				Observable.interval(20, TimeUnit.MILLISECONDS).forEach(n -> {
-					frame.getMapPane().repaint();
-				});
-			});
-	}
+                });
 
-	private Layer createCoastlineLayer() {
-		try {
-			// File file = new File(
-			// "/home/dxm/Downloads/shapefile-australia-coastline-polygon/cstauscd_r.shp");
-			File file = new File("src/main/resources/shapes/countries.shp");
-			FileDataStore store = FileDataStoreFinder.getDataStore(file);
-			SimpleFeatureSource featureSource = store.getFeatureSource();
+                frame.setVisible(true);
+                Observable.interval(20, TimeUnit.MILLISECONDS).forEach(n -> {
+                    frame.getMapPane().repaint();
+                });
+            });
+    }
 
-			Style style = SLD.createSimpleStyle(featureSource.getSchema());
-			Layer layer = new FeatureLayer(featureSource, style);
-			return layer;
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    private Layer createCoastlineLayer() {
+        try {
+            // File file = new File(
+            // "/home/dxm/Downloads/shapefile-australia-coastline-polygon/cstauscd_r.shp");
+            File file = new File("src/main/resources/shapes/countries.shp");
+            FileDataStore store = FileDataStoreFinder.getDataStore(file);
+            SimpleFeatureSource featureSource = store.getFeatureSource();
 
-	private static Layer createExtraFeatures() {
-		SimpleFeatureTypeBuilder b = new SimpleFeatureTypeBuilder();
-		b.setName("Location");
-		b.setCRS(DefaultGeographicCRS.WGS84);
-		// picture location
-		b.add("geom", Point.class);
-		final SimpleFeatureType TYPE = b.buildFeatureType();
+            Style style = SLD.createSimpleStyle(featureSource.getSchema());
+            Layer layer = new FeatureLayer(featureSource, style);
+            return layer;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-		GeometryFactory gf = JTSFactoryFinder.getGeometryFactory();
-		Point point = gf
-				.createPoint(new Coordinate(CANBERRA_LONG, CANBERRA_LAT));
+    private static Layer createExtraFeatures() {
+        SimpleFeatureTypeBuilder b = new SimpleFeatureTypeBuilder();
+        b.setName("Location");
+        b.setCRS(DefaultGeographicCRS.WGS84);
+        // picture location
+        b.add("geom", Point.class);
+        final SimpleFeatureType TYPE = b.buildFeatureType();
 
-		SimpleFeatureBuilder builder = new SimpleFeatureBuilder(TYPE);
-		builder.add(point);
-		SimpleFeature feature = builder.buildFeature("Canberra");
-		DefaultFeatureCollection features = new DefaultFeatureCollection(null,
-				null);
-		features.add(feature);
+        GeometryFactory gf = JTSFactoryFinder.getGeometryFactory();
+        Point point = gf.createPoint(new Coordinate(CANBERRA_LONG, CANBERRA_LAT));
 
-		Style style = SLD.createPointStyle("Star", Color.BLUE, Color.BLUE,
-				0.3f, 10);
+        SimpleFeatureBuilder builder = new SimpleFeatureBuilder(TYPE);
+        builder.add(point);
+        SimpleFeature feature = builder.buildFeature("Canberra");
+        DefaultFeatureCollection features = new DefaultFeatureCollection(null, null);
+        features.add(feature);
 
-		return new FeatureLayer(features, style);
-	}
+        Style style = SLD.createPointStyle("Star", Color.BLUE, Color.BLUE, 0.3f, 10);
 
-	static void addWms(MapContent map) {
-		// URL wmsUrl = WMSChooser.showChooseWMS();
+        return new FeatureLayer(features, style);
+    }
 
-		WebMapServer wms;
-		try {
-			String url = "http://129.206.228.72/cached/osm?Request=GetCapabilities";
-			// String url = "http://sarapps.amsa.gov.au:8080/cts-gis/wms";
-			wms = new WebMapServer(new URL(url));
-		} catch (ServiceException | IOException e) {
-			throw new RuntimeException(e);
-		}
-		List<org.geotools.data.ows.Layer> wmsLayers = WMSLayerChooser
-				.showSelectLayer(wms);
-		for (org.geotools.data.ows.Layer wmsLayer : wmsLayers) {
-			System.out.println("adding " + wmsLayer.getTitle());
-			WMSLayer displayLayer = new WMSLayer(wms, wmsLayer);
-			map.addLayer(displayLayer);
-		}
-	}
+    static void addWms(MapContent map) {
+        // URL wmsUrl = WMSChooser.showChooseWMS();
 
-	public static void main(String[] args) throws Exception {
-		System.setProperty("http.proxyHost", "proxy.amsa.gov.au");
-		System.setProperty("http.proxyPort", "8080");
-		System.setProperty("https.proxyHost", "proxy.amsa.gov.au");
-		System.setProperty("https.proxyPort", "8080");
-		new Animator().start();
-	}
+        WebMapServer wms;
+        try {
+            String url = "http://129.206.228.72/cached/osm?Request=GetCapabilities";
+            // String url = "http://sarapps.amsa.gov.au:8080/cts-gis/wms";
+            wms = new WebMapServer(new URL(url));
+        } catch (ServiceException | IOException e) {
+            throw new RuntimeException(e);
+        }
+        List<org.geotools.data.ows.Layer> wmsLayers = WMSLayerChooser.showSelectLayer(wms);
+        for (org.geotools.data.ows.Layer wmsLayer : wmsLayers) {
+            System.out.println("adding " + wmsLayer.getTitle());
+            WMSLayer displayLayer = new WMSLayer(wms, wmsLayer);
+            map.addLayer(displayLayer);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty("http.proxyHost", "proxy.amsa.gov.au");
+        System.setProperty("http.proxyPort", "8080");
+        System.setProperty("https.proxyHost", "proxy.amsa.gov.au");
+        System.setProperty("https.proxyPort", "8080");
+        new Animator().start();
+    }
 
 }
