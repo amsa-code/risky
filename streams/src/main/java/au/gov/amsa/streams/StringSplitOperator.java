5292104d1052aee00d26f17964fe173244bd1370
Dave Moten <davidmoten@gmail.com>
2015-06-16 17:02:58 +1000

    fix StringSplitOperator that could recurse via requests

--- streams/src/main/java/au/gov/amsa/streams/StringSplitOperator.java
+++ streams/src/main/java/au/gov/amsa/streams/StringSplitOperator.java
@@ -1,18 +1,15 @@
 package au.gov.amsa.streams;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.concurrent.atomic.AtomicLongFieldUpdater;
 import java.util.regex.Pattern;
 
 import rx.Observable.Operator;
-import rx.Producer;
 import rx.Subscriber;
-import rx.internal.operators.NotificationLite;
+
+import com.github.davidmoten.rx.operators.OperatorBufferEmissions;
 
 /**
  * Splits and joins items in a sequence of strings based on a regex pattern.
- * Supports backpressure.
+ * Supports backpressure when combined with {@link OperatorBufferEmissions}.
  */
 public final class StringSplitOperator implements Operator<String, String> {
 
@@ -32,142 +29,48 @@ public final class StringSplitOperator implements Operator<String, String> {
 
     @Override
     public Subscriber<? super String> call(Subscriber<? super String> child) {
-        final ParentSubscriber parent = new ParentSubscriber(child, pattern);
-        child.setProducer(new Producer() {
+        return new Subscriber<String>(child) {
+
+            // the bit left over from that last string that hasn't been
+            // terminated yet
+            private String leftOver = null;
+
             @Override
-            public void request(long n) {
-                parent.requestMore(n);
-            }
-        });
-
-        // ensure unsubscription of child unsubscribes parent
-        child.add(parent);
-        return parent;
-    }
-
-    private static final class ParentSubscriber extends Subscriber<String> {
-
-        // utility object for reactive events
-        private final NotificationLite<String> on = NotificationLite.instance();
-
-        // report events to this downstream subscriber
-        private final Subscriber<? super String> child;
-
-        // fast path or not
-        private volatile boolean requestAll = false;
-
-        // number of items being waited for (not used if fast path selected)
-        private volatile long expected = 0;
-
-        // updater object for the expected field above
-        private final AtomicLongFieldUpdater<ParentSubscriber> EXPECTED = AtomicLongFieldUpdater
-                .newUpdater(ParentSubscriber.class, "expected");
-
-        // queue to hold messages till they are requested
-        private final Deque<Object> queue = new LinkedList<Object>();
-
-        // pattern to split upon
-        private final Pattern pattern;
-
-        // the bit left over from that last string that hasn't been terminated
-        // yet
-        private String leftOver = null;
-
-        private ParentSubscriber(Subscriber<? super String> child, Pattern pattern) {
-            this.child = child;
-            this.pattern = pattern;
-        }
-
-        private void requestMore(long n) {
-            // this method may be run concurrent with any of the event methods
-            // below so watch out for thread safety
-            if (requestAll || n <= 0)
-                // ignore request if all items have already been requested or if
-                // invalid request is received
-                return;
-            else if (n == Long.MAX_VALUE)
-                requestAll = true;
-            else
-                EXPECTED.addAndGet(this, n);
-            request(n);
-            drainQueue();
-        }
-
-        @Override
-        public void onCompleted() {
-            if (requestAll) {
+            public void onCompleted() {
                 // fast path
                 if (leftOver != null)
                     child.onNext(leftOver);
                 if (!isUnsubscribed()) {
                     child.onCompleted();
                 }
-            } else {
-                // backpressure path
-                if (leftOver != null)
-                    queue.add(leftOver);
-                queue.add(on.completed());
-                drainQueue();
             }
-        }
 
-        @Override
-        public void onError(Throwable e) {
-            if (requestAll) {
+            @Override
+            public void onError(Throwable e) {
                 // fast path
                 child.onError(e);
-            } else {
-                // backpressure path
-                queue.add(on.error(e));
-                drainQueue();
             }
-        }
 
-        @Override
-        public void onNext(String s) {
-            String[] parts = pattern.split(s, -1);
-            // prepend leftover to the first part
-            if (leftOver != null)
-                parts[0] = leftOver + parts[0];
+            @Override
+            public void onNext(String s) {
+                String[] parts = pattern.split(s, -1);
+                // prepend leftover to the first part
+                if (leftOver != null)
+                    parts[0] = leftOver + parts[0];
 
-            // can emit all parts except the last part because it hasn't been
-            // terminated by the pattern/end-of-stream yet
-            if (requestAll) {
-                // fast path
+                // can emit all parts except the last part because it hasn't
+                // been terminated by the pattern/end-of-stream yet
                 for (int i = 0; i < parts.length - 1; i++)
                     child.onNext(parts[i]);
-            } else {
-                // backpressure path
-                for (int i = 0; i < parts.length - 1; i++)
-                    queue.add(on.next(parts[i]));
-                if (parts.length == 1)
-                    request(1);
-                drainQueue();
-            }
-            // we have to assign the last part as leftOver because we don't know
-            // if it has been terminated yet
-            leftOver = parts[parts.length - 1];
-        }
 
-        private void drainQueue() {
-            // only used by backpressure path
-            while (true) {
-                Object item = queue.peek();
-                if (item == null || isUnsubscribed())
-                    break;
-                else if (on.isCompleted(item) || on.isError(item)) {
-                    on.accept(child, queue.poll());
-                    break;
-                } else if (expected == 0)
-                    break;
-                else {
-                    // expected won't be Long.MAX_VALUE so can safely
-                    // decrement
-                    EXPECTED.decrementAndGet(this);
-                    on.accept(child, queue.poll());
-                }
+                if (parts.length == 1)
+                    // ensure that the stream does not stall
+                    request(1);
+                // we have to assign the last part as leftOver because we
+                // don't know if it has been terminated yet
+                leftOver = parts[parts.length - 1];
             }
-        }
+        };
     }
 
 }
--- streams/src/main/java/au/gov/amsa/streams/Strings.java
+++ streams/src/main/java/au/gov/amsa/streams/Strings.java
@@ -14,6 +14,8 @@ import rx.functions.Action1;
 import rx.functions.Func0;
 import rx.functions.Func1;
 
+import com.github.davidmoten.rx.operators.OperatorBufferEmissions;
+
 /**
  * Utilities for stream processing of lines of text from
  * <ul>
@@ -22,71 +24,72 @@ import rx.functions.Func1;
  */
 public final class Strings {
 
-	public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+    public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
 
-	/**
-	 * Returns null if input is null otherwise returns input.toString().trim().
-	 */
-	public static Func1<Object, String> TRIM = new Func1<Object, String>() {
+    /**
+     * Returns null if input is null otherwise returns input.toString().trim().
+     */
+    public static Func1<Object, String> TRIM = new Func1<Object, String>() {
 
-		@Override
-		public String call(Object input) {
-			if (input == null)
-				return null;
-			else
-				return input.toString().trim();
-		}
-	};
+        @Override
+        public String call(Object input) {
+            if (input == null)
+                return null;
+            else
+                return input.toString().trim();
+        }
+    };
 
-	public static Observable<String> from(final Reader reader, final int size) {
-		return new OnSubscribeReader(reader, size).toObservable();
-		// return StringObservable.from(reader, size);
-	}
+    public static Observable<String> from(final Reader reader, final int size) {
+        return new OnSubscribeReader(reader, size).toObservable();
+        // return StringObservable.from(reader, size);
+    }
 
-	public static Observable<String> from(Reader reader) {
-		return from(reader, 8192);
-	}
+    public static Observable<String> from(Reader reader) {
+        return from(reader, 8192);
+    }
 
-	public static Observable<String> lines(Reader reader) {
-		return split(from(reader, 8192), "\n");
-	}
+    public static Observable<String> lines(Reader reader) {
+        return split(from(reader, 8192), "\n");
+    }
 
-	public static Observable<String> split(Observable<String> source, String pattern) {
-		return source.lift(new StringSplitOperator(Pattern.compile(pattern)));
-	}
+    public static Observable<String> split(Observable<String> source, String pattern) {
+        return source.lift(new StringSplitOperator(Pattern.compile(pattern))).lift(
+                new OperatorBufferEmissions<String>());
+    }
 
-	public static Observable<String> from(File file) {
-		return from(file, DEFAULT_CHARSET);
-	}
+    public static Observable<String> from(File file) {
+        return from(file, DEFAULT_CHARSET);
+    }
 
-	public static Observable<String> from(final File file, final Charset charset) {
-		Func0<Reader> resourceFactory = new Func0<Reader>() {
-			@Override
-			public Reader call() {
-				try {
-					return new InputStreamReader(new FileInputStream(file), charset);
-				} catch (FileNotFoundException e) {
-					throw new RuntimeException(e);
-				}
-			}
-		};
-		Func1<Reader, Observable<String>> observableFactory = new Func1<Reader, Observable<String>>() {
-			@Override
-			public Observable<String> call(Reader is) {
-				return from(is);
-			}
-		};
-		Action1<Reader> disposeAction = new Action1<Reader>() {
-			@Override
-			public void call(Reader is) {
-				try {
-					is.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		};
-		return Observable.using(resourceFactory, observableFactory, disposeAction, true);
-	}
+    public static Observable<String> from(final File file, final Charset charset) {
+        Func0<Reader> resourceFactory = new Func0<Reader>() {
+            @Override
+            public Reader call() {
+                try {
+                    return new InputStreamReader(new FileInputStream(file), charset);
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        Func1<Reader, Observable<String>> observableFactory = new Func1<Reader, Observable<String>>() {
+            @Override
+            public Observable<String> call(Reader is) {
+                return from(is);
+            }
+        };
+        Action1<Reader> disposeAction = new Action1<Reader>() {
+            @Override
+            public void call(Reader is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        return Observable.using(resourceFactory, observableFactory, disposeAction, true);
+    }
 
 }
