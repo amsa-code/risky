7d403222810a7d63261351520496288765777028
Dave Moten <davidmoten@gmail.com>
2015-06-19 15:11:31 +1000

    shapefile to geojson conversion

--- geotools-extras/src/main/java/au/gov/amsa/gt/Shapefile.java
+++ geotools-extras/src/main/java/au/gov/amsa/gt/Shapefile.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,7 +17,15 @@ import org.geotools.data.DataStoreFinder;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.geojson.feature.FeatureJSON;
+import org.geotools.geometry.jts.JTS;
+import org.geotools.referencing.CRS;
 import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
@@ -26,97 +35,121 @@ import com.vividsolutions.jts.geom.prep.PreparedGeometryFactory;
 
 public final class Shapefile {
 
-	private final DataStore datastore;
-	private final static GeometryFactory GEOMETRY_FACTORY = new GeometryFactory();
-	// 0 = not loaded, not closed, 1 = loaded, 2 = closed
-	private final AtomicInteger state = new AtomicInteger(0);
-	private static final int NOT_LOADED = 0;
-	private static final int LOADED = 1;
-	private static final int CLOSED = 2;
+    private final DataStore datastore;
+    private final static GeometryFactory GEOMETRY_FACTORY = new GeometryFactory();
+    // 0 = not loaded, not closed, 1 = loaded, 2 = closed
+    private final AtomicInteger state = new AtomicInteger(0);
+    private static final int NOT_LOADED = 0;
+    private static final int LOADED = 1;
+    private static final int CLOSED = 2;
 
-	private volatile List<PreparedGeometry> geometries;
-	private final double bufferDistance;
+    private volatile List<PreparedGeometry> geometries;
+    private final double bufferDistance;
+    private volatile List<SimpleFeature> features = new ArrayList<SimpleFeature>();
+    private volatile CoordinateReferenceSystem crs;
 
-	private Shapefile(File file, double bufferDistance) {
-		this.bufferDistance = bufferDistance;
-		try {
-			Map<String, Serializable> map = new HashMap<>();
-			map.put("url", file.toURI().toURL());
-			datastore = DataStoreFinder.getDataStore(map);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    private Shapefile(File file, double bufferDistance) {
+        this.bufferDistance = bufferDistance;
+        try {
+            Map<String, Serializable> map = new HashMap<>();
+            map.put("url", file.toURI().toURL());
+            datastore = DataStoreFinder.getDataStore(map);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public static Shapefile from(File file) {
-		return from(file, 0);
-	}
+    public static Shapefile from(File file) {
+        return from(file, 0);
+    }
 
-	public static Shapefile from(File file, double bufferDistance) {
-		return new Shapefile(file, bufferDistance);
-	}
+    public static Shapefile from(File file, double bufferDistance) {
+        return new Shapefile(file, bufferDistance);
+    }
 
-	public static void createPolygon(List<Coordinate> coords, File output) {
-		ShapefileCreator.createPolygon(coords, output);
-	}
+    public static void createPolygon(List<Coordinate> coords, File output) {
+        ShapefileCreator.createPolygon(coords, output);
+    }
 
-	public static Shapefile fromZip(InputStream is) {
-		return fromZip(is, 0);
-	}
+    public static Shapefile fromZip(InputStream is) {
+        return fromZip(is, 0);
+    }
 
-	public static Shapefile fromZip(InputStream is, double bufferDistance) {
-		try {
-			File directory = Files.createTempDirectory("shape-").toFile();
-			ZipUtil.unzip(is, directory);
-			return new Shapefile(directory, bufferDistance);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public static Shapefile fromZip(InputStream is, double bufferDistance) {
+        try {
+            File directory = Files.createTempDirectory("shape-").toFile();
+            ZipUtil.unzip(is, directory);
+            return new Shapefile(directory, bufferDistance);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	private Shapefile load() {
-		if (state.compareAndSet(NOT_LOADED, LOADED)) {
-			try {
-				final List<PreparedGeometry> geometries = new ArrayList<>();
-				for (String typeName : datastore.getTypeNames()) {
-					SimpleFeatureSource source = datastore
-							.getFeatureSource(typeName);
-					final SimpleFeatureCollection features = source
-							.getFeatures();
-					SimpleFeatureIterator it = features.features();
-					while (it.hasNext()) {
-						SimpleFeature feature = it.next();
-						Geometry g = (Geometry) feature.getDefaultGeometry();
-						if (bufferDistance > 0)
-							g = g.buffer(bufferDistance);
-						geometries.add(PreparedGeometryFactory.prepare(g));
-					}
-					it.close();
-				}
-				this.geometries = geometries;
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-		} else if (state.get() == CLOSED)
-			throw new RuntimeException(
-					"Shapefile is closed and can't be accessed");
-		return this;
-	}
+    private Shapefile load() {
+        if (state.compareAndSet(NOT_LOADED, LOADED)) {
+            try {
+                final List<PreparedGeometry> geometries = new ArrayList<>();
+                for (String typeName : datastore.getTypeNames()) {
+                    SimpleFeatureSource source = datastore.getFeatureSource(typeName);
+                    crs = source.getBounds().getCoordinateReferenceSystem();
+                    final SimpleFeatureCollection features = source.getFeatures();
+                    SimpleFeatureIterator it = features.features();
+                    while (it.hasNext()) {
+                        SimpleFeature feature = it.next();
+                        Geometry g = (Geometry) feature.getDefaultGeometry();
+                        if (bufferDistance > 0)
+                            g = g.buffer(bufferDistance);
+                        geometries.add(PreparedGeometryFactory.prepare(g));
+                        this.features.add(feature);
+                    }
+                    it.close();
+                }
+                this.geometries = geometries;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (state.get() == CLOSED)
+            throw new RuntimeException("Shapefile is closed and can't be accessed");
+        return this;
+    }
 
-	public List<PreparedGeometry> geometries() {
-		load();
-		return geometries;
-	}
+    public List<PreparedGeometry> geometries() {
+        load();
+        return geometries;
+    }
 
-	public boolean contains(double lat, double lon) {
-		load();
-		return GeometryUtil.contains(GEOMETRY_FACTORY, geometries, lat, lon);
-	}
+    public boolean contains(double lat, double lon) {
+        load();
+        return GeometryUtil.contains(GEOMETRY_FACTORY, geometries, lat, lon);
+    }
 
-	public void close() {
-		if (state.compareAndSet(NOT_LOADED, CLOSED)
-				|| state.compareAndSet(LOADED, CLOSED))
-			datastore.dispose();
-	}
+    public void close() {
+        if (state.compareAndSet(NOT_LOADED, CLOSED) || state.compareAndSet(LOADED, CLOSED))
+            datastore.dispose();
+    }
 
+    public void writeGeoJson(Writer writer, String targetCrsName) {
+        load();
+
+        try {
+            CoordinateReferenceSystem targetCrs = CRS.decode(targetCrsName);
+            MathTransform transform = CRS.findMathTransform(crs, targetCrs);
+            FeatureJSON f = new FeatureJSON();
+            f.writeCRS(targetCrs, writer);
+
+            features.stream().forEach(feature -> {
+                try {
+                    SimpleFeatureTypeBuilder tb = new SimpleFeatureTypeBuilder();
+                    tb.setCRS(targetCrs);
+                    SimpleFeatureBuilder b = new SimpleFeatureBuilder(feature.getFeatureType());
+                    b.add(JTS.transform((Geometry) feature.getDefaultGeometry(), transform));
+                    f.writeFeature(b.buildFeature(feature.getID()), writer);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (IOException | FactoryException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- geotools-extras/src/test/java/au/gov/amsa/gt/ShapefileTest.java
+++ geotools-extras/src/test/java/au/gov/amsa/gt/ShapefileTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
@@ -22,78 +23,80 @@ import com.vividsolutions.jts.geom.Point;
 
 public class ShapefileTest {
 
-	@Test
-	public void testLoadShapefileFromFileSystem() {
-		Shapefile shape = Shapefile.from(new File(
-				"src/test/resources/shapefile-srr-polygon/srr.shp"));
-		assertTrue(shape.contains(-20, 135));
-		assertFalse(shape.contains(0, 0));
-		assertEquals(4, shape.geometries().size());
-		shape.close();
+    @Test
+    public void testLoadShapefileFromFileSystem() {
+        Shapefile shape = Shapefile.from(new File(
+                "src/test/resources/shapefile-srr-polygon/srr.shp"));
+        assertTrue(shape.contains(-20, 135));
+        assertFalse(shape.contains(0, 0));
+        assertEquals(4, shape.geometries().size());
+        shape.close();
 
-	}
+    }
 
-	@Test
-	public void testLoadShapefileFromZippedInputStream() throws Exception {
-		Shapefile shape = Shapefile.fromZip(Shapefile.class
-				.getResourceAsStream("/shapefile-srr-polygon.zip"));
-		assertFalse(shape.contains(0, 0));
-		assertEquals(4, shape.geometries().size());
-		shape.close();
-	}
+    @Test
+    public void testLoadShapefileFromZippedInputStream() throws Exception {
+        Shapefile shape = Shapefile.fromZip(Shapefile.class
+                .getResourceAsStream("/shapefile-srr-polygon.zip"));
+        assertFalse(shape.contains(0, 0));
+        assertEquals(4, shape.geometries().size());
+        shape.close();
+    }
 
-	@Test
-	public void testContainsInSrr() {
-		// northern border is -12, 113
-		Shapefile shape = Shapefile.fromZip(Shapefile.class
-				.getResourceAsStream("/shapefile-srr-polygon.zip"));
+    @Test
+    public void testContainsInSrr() {
+        // northern border is -12, 113
+        Shapefile shape = Shapefile.fromZip(Shapefile.class
+                .getResourceAsStream("/shapefile-srr-polygon.zip"));
 
-		assertTrue(shape.contains(-13, 113));
-		assertFalse(shape.contains(-10, 113));
-	}
+        assertTrue(shape.contains(-13, 113));
+        assertFalse(shape.contains(-10, 113));
+    }
 
-	@Test
-	public void testShowDistanceBetweenPoints() {
-		GeometryFactory gf = JTSFactoryFinder.getGeometryFactory();
-		Point a = gf.createPoint(new Coordinate(-10, 114.0));
-		Point b = gf.createPoint(new Coordinate(-11, 113.0));
-		System.out.println("distance=" + a.distance(b));
-	}
+    @Test
+    public void testShowDistanceBetweenPoints() {
+        GeometryFactory gf = JTSFactoryFinder.getGeometryFactory();
+        Point a = gf.createPoint(new Coordinate(-10, 114.0));
+        Point b = gf.createPoint(new Coordinate(-11, 113.0));
+        System.out.println("distance=" + a.distance(b));
+    }
 
-	@Test
-	public void testContainsInSrrWithBuffer() {
-		// northern border is -12, 113
-		Shapefile shape = Shapefile.fromZip(Shapefile.class
-				.getResourceAsStream("/shapefile-srr-polygon.zip"), 3);
+    @Test
+    public void testContainsInSrrWithBuffer() {
+        // northern border is -12, 113
+        Shapefile shape = Shapefile.fromZip(
+                Shapefile.class.getResourceAsStream("/shapefile-srr-polygon.zip"), 3);
 
-		assertTrue(shape.contains(-13, 113));
-		assertTrue(shape.contains(-10, 113));
-		assertFalse(shape.contains(-8, 113));
-	}
+        assertTrue(shape.contains(-13, 113));
+        assertTrue(shape.contains(-10, 113));
+        assertFalse(shape.contains(-8, 113));
+    }
 
-	@Test
-	public void testCreatePolygonShapefile() throws IOException {
-		try (InputStreamReader r = new InputStreamReader(
-				Shapefile.class.getResourceAsStream("/southwest.txt"))) {
-			// read coordinates from classpath
-			List<Coordinate> coords = Strings
-					.lines(r)
-					.map(line -> line.trim())
-					.filter(line -> line.length() > 0)
-					.filter(line -> line.charAt(0) != '#')
-					.doOnNext(System.out::println)
-					.map(line -> line.split(" "))
-					.map(items -> new Coordinate(parseDouble(items[0]),
-							parseDouble(items[1])))
-					.toList()
-					.map(list -> {
-						List<Coordinate> list2 = Lists.newArrayList(list);
-						list2.add(new Coordinate(list.get(0).x, list.get(0).y));
-						return list2;
-					}).toBlocking().single();
-			ShapefileCreator.createPolygon(coords, new File(
-					"target/southwest.shp"));
-		}
-	}
+    @Test
+    public void testCreatePolygonShapefile() throws IOException {
+        try (InputStreamReader r = new InputStreamReader(
+                Shapefile.class.getResourceAsStream("/southwest.txt"))) {
+            // read coordinates from classpath
+            List<Coordinate> coords = Strings.lines(r).map(line -> line.trim())
+                    .filter(line -> line.length() > 0).filter(line -> line.charAt(0) != '#')
+                    .doOnNext(System.out::println).map(line -> line.split(" "))
+                    .map(items -> new Coordinate(parseDouble(items[0]), parseDouble(items[1])))
+                    .toList().map(list -> {
+                        List<Coordinate> list2 = Lists.newArrayList(list);
+                        list2.add(new Coordinate(list.get(0).x, list.get(0).y));
+                        return list2;
+                    }).toBlocking().single();
+            ShapefileCreator.createPolygon(coords, new File("target/southwest.shp"));
+        }
+    }
+
+    @Test
+    public void testToGeoJson() throws IOException {
+        Shapefile shape = Shapefile.fromZip(
+                Shapefile.class.getResourceAsStream("/shapefile-srr-polygon.zip"), 3);
+        FileWriter writer = new FileWriter("target/srr.geojson");
+        shape.writeGeoJson(writer, "EPSG:4326");
+        writer.close();
+    }
 
 }
