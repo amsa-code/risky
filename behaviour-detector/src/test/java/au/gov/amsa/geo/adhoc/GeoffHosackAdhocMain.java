ce8e61391e5d77ba93c372423430eb0a76bbefc4
Dave Moten <davidmoten@gmail.com>
2015-07-07 11:50:08 +1000

    geoff dev

--- behaviour-detector/src/test/java/au/gov/amsa/geo/adhoc/GeoffHosackAdhocMain.java
+++ behaviour-detector/src/test/java/au/gov/amsa/geo/adhoc/GeoffHosackAdhocMain.java
@@ -4,18 +4,21 @@ import java.io.File;
 import java.util.regex.Pattern;
 
 import rx.Observable;
+import rx.functions.Func2;
+import rx.schedulers.Schedulers;
 import au.gov.amsa.risky.format.BinaryFixes;
 import au.gov.amsa.util.Files;
 import au.gov.amsa.util.Pair;
 
-import com.github.davidmoten.rx.slf4j.Logging;
-
 public class GeoffHosackAdhocMain {
 
     public static void main(String[] args) {
-        int option = 2;
+        int option = 1;
         if (option == 1) {
-            File file = new File("/media/an/binary-fixes-5-minute/2014/235099876.track");
+            String filename = "/home/dxm/temp/235099876.track";
+            // String filename =
+            // "/media/an/binary-fixes-5-minute/2014/235099876.track";
+            File file = new File(filename);
             BinaryFixes.from(file).doOnNext(System.out::println)
                     .toSortedList((a, b) -> Long.compare(a.time(), b.time()))
                     .flatMap(Observable::from).doOnCompleted(() -> System.out.println("complete"))
@@ -24,20 +27,26 @@ public class GeoffHosackAdhocMain {
             Observable<File> files = Observable.from(Files.find(new File(
                     "/media/an/binary-fixes-5-minute/2014"), Pattern.compile("\\d+\\.track")));
 
+            Func2<Pair<File, Long>, Pair<File, Long>, Pair<File, Long>> earliest = (p1, p2) -> {
+                if (p1.b() <= p2.b())
+                    return p1;
+                else
+                    return p2;
+            };
             files.filter(file -> !file.getName().equals("0.track"))
-                    //
+                    // read fixes from file
                     .flatMap(
-                            file -> BinaryFixes.from(file).<Pair<File, Long>> map(
-                                    fix -> Pair.create(file, fix.time())))
-                    // log
-                    .lift(Logging.<Pair<File, Long>> logger().showCount().every(1000000).log())
+                            file -> BinaryFixes.from(file)
+                                    .<Pair<File, Long>> map(fix -> Pair.create(file, fix.time()))
+                                    .reduce(earliest)
+                                    .doOnNext(p -> System.out.println(file + ": " + p))
+                                    .subscribeOn(Schedulers.computation()))
                     //
-                    .reduce((p1, p2) -> {
-                        if (p1.b() <= p2.b())
-                            return p1;
-                        else
-                            return p2;
-                    }).doOnNext(System.out::println).subscribe();
+                    .reduce(earliest)
+                    // print answer
+                    .doOnNext(System.out::println)
+                    // go
+                    .toBlocking().single();
         }
     }
 }
