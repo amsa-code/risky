e5ddc46e145884f9b01ad7ddad33347d1cdfc645
Dave Moten <davidmoten@gmail.com>
2015-07-29 09:55:10 +1000

    add NetCdfWriter

--- formats/src/main/java/au/gov/amsa/util/netcdf/NetCdfWriter.java
+++ formats/src/main/java/au/gov/amsa/util/netcdf/NetCdfWriter.java
@@ -4,10 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 
+import ucar.ma2.Array;
 import ucar.ma2.DataType;
+import ucar.ma2.InvalidRangeException;
 import ucar.nc2.Attribute;
 import ucar.nc2.Dimension;
 import ucar.nc2.NetcdfFileWriter;
@@ -15,12 +17,13 @@ import ucar.nc2.NetcdfFileWriter.Version;
 import ucar.nc2.Variable;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
 
 public class NetCdfWriter {
 
     private final NetcdfFileWriter f;
-    private final Map<String, Var<?, ?>> variables = new HashMap<>();
     private final int numRecords;
+    private final Map<Var<?>, List<?>> map = new HashMap<>();
 
     public NetCdfWriter(File file, int numRecords) {
         this.numRecords = numRecords;
@@ -33,43 +36,69 @@ public class NetCdfWriter {
         }
     }
 
-    public <T, R> Var<T, R> addVariable(String standardName, Optional<String> longName,
-            Optional<String> units, Optional<String> encoding, Class<R> cls,
-            Function<? super T, R> function) {
+    public <T> Var<T> addVariable(String standardName, Optional<String> longName,
+            Optional<String> units, Optional<String> encoding, Class<T> cls) {
         Dimension dimension = f.addDimension(null, standardName, numRecords);
         Variable variable = f.addVariable(null, standardName, toDataType(cls),
                 Arrays.asList(dimension));
         if (units.isPresent())
             variable.addAttribute(new Attribute("units", units.get()));
-        if (units.isPresent())
+        if (encoding.isPresent())
             variable.addAttribute(new Attribute("encoding", encoding.get()));
-        return new Var<T, R>(variable, function);
+        return new Var<T>(variable, cls);
     }
 
-    public <T, R> void write(Var<T, R> variable, T record) {
+    public <T> void add(Var<T> variable, T value) {
+        @SuppressWarnings("unchecked")
+        List<Object> list = (List<Object>) map.get(variable);
+        if (list == null) {
+            list = Lists.newArrayList();
+            map.put(variable, list);
+        }
+        list.add(value);
+    }
 
+    public void close() {
+
+        try {
+            f.create();
+            for (Var<?> var : map.keySet()) {
+                List<?> list = map.get(var);
+                int[] shape = new int[] { list.size() };
+                Array data = Array.factory(DataType.getType(var.cls()), shape);
+                for (int i = 0; i < list.size(); i++) {
+                    data.setObject(i, list.get(i));
+                }
+                f.write(var.variable(), data);
+            }
+            f.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidRangeException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static DataType toDataType(Class<?> cls) {
         return DataType.DOUBLE;
     }
 
-    public static class Var<T, R> {
+    public static class Var<T> {
 
         private final Variable variable;
-        private final Function<? super T, R> function;
+        private final Class<T> cls;
 
-        public Var(Variable variable, Function<? super T, R> function) {
+        public Var(Variable variable, Class<T> cls) {
             this.variable = variable;
-            this.function = function;
+            this.cls = cls;
         }
 
         public Variable variable() {
             return variable;
         }
 
-        public Function<? super T, R> function() {
-            return function;
+        public Class<T> cls() {
+            return cls;
         }
 
     }
