1c9f8dc7b170024f65f0b9016b8aa9338c7fd00d
Dave Moten <davidmoten@gmail.com>
2015-09-18 17:14:43 +1000

    daily conversion

--- formats/src/test/java/au/gov/amsa/risky/format/ByMmsiToDailyMain.java
+++ formats/src/test/java/au/gov/amsa/risky/format/ByMmsiToDailyMain.java
@@ -5,8 +5,8 @@ import java.io.File;
 public class ByMmsiToDailyMain {
 
     public static void main(String[] args) {
-        System.setProperty("input", "/media/an/binary-fixes-5-minute/2014");
-        System.setProperty("output", "/media/an/daily-fixes-5-minute/2014");
+        System.setProperty("input", "/media/an/binary-fixes/2014");
+        System.setProperty("output", "/media/an/daily-fixes/2014");
         File input = new File(System.getProperty("input"));
         File output = new File(System.getProperty("output"));
         ByMmsiToDailyConverter.convert(input, output);
