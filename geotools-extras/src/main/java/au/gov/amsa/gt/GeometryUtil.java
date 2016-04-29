6d149e64544eee51863c768b60b0d6f4b050520b
Dave Moten <davidmoten@gmail.com>
2016-04-29 14:32:58 +1000

    minor perf improvement in GeometryUtil

--- geotools-extras/src/main/java/au/gov/amsa/gt/GeometryUtil.java
+++ geotools-extras/src/main/java/au/gov/amsa/gt/GeometryUtil.java
@@ -4,17 +4,18 @@ import java.util.Collection;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.prep.PreparedGeometry;
 
 public final class GeometryUtil {
 
     public static boolean contains(GeometryFactory gf, Collection<PreparedGeometry> geometries,
             double lat, double lon) {
+        Point point = gf.createPoint(new Coordinate(lon, lat));
         for (PreparedGeometry g : geometries) {
-            if (g.contains(gf.createPoint(new Coordinate(lon, lat))))
+            if (g.contains(point))
                 return true;
         }
         return false;
     }
-
 }
