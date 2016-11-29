7ed5299135f736055936c07a9a319d8dfe355141
Dave Moten <davidmoten@gmail.com>
2016-11-29 12:41:58 +1100

    update scripts and revert to deploy to amsa-maven instead of central

---
+++ release-to-amsa.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+set -x
+if [ $# -eq 0 ]; then
+    echo "Usage: ./release.sh <VERSION>"
+    exit 1
+fi
+VERSION=$1
+git checkout master
+git pull
+mvn versions:set -DnewVersion=$VERSION -DgenerateBackupPoms=false
+mvn clean install 
+git commit -am "prepare for release $VERSION"
+git push
+git tag -a $VERSION -m "$VERSION"
+git push origin $VERSION
+git checkout $VERSION
+mvn deploy -Dmode=test -Drisky.repo=amsa-maven -Drisky.repo.url=http://maven.amsa.gov.au:8081/artifactory/libs-releases-local
+git checkout master
+mvn versions:set -DnewVersion=$VERSION.1-SNAPSHOT -DgenerateBackupPoms=false
+git commit -am "setting versions to snapshot"
+git push
+git checkout $VERSION
+./generate-site.sh
+git checkout master
+
+## could not get release plugin to work for git push!
+#GIT_USERNAME=$2
+#GIT_PASSWORD=$3
+#DRY_RUN=false
+#git pull
+#mvn release:clean
+#mvn -B -X release:prepare -DreleaseVersion=$VERSION -DautoVersionSubmodules=true -Dtag=$VERSION -DdryRun=#$DRY_RUN -Dusername=$GIT_USERNAME -Dpassword=$GIT_PASSWORD
+#mvn release:perform -X -Darguments='-Drisky.repo=amsa-maven -Dmode=test -Drisky.repo.url=http://sardevc.amsa.gov.au:8081/artifactory/libs-releases-local' -DdryRun=$DRY_RUN -Dusername=$GIT_USERNAME -#Dpassword=$GIT_PASSWORD
