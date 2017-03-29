3d2d48d8697da771c8b46a827dad6808de045480
Dave Moten <davidmoten@gmail.com>
2017-03-30 10:26:35 +1100

    update to handle mariweb saas

--- ais/src/test/java/au/gov/amsa/ais/MariwebSslConnectMain.java
+++ ais/src/test/java/au/gov/amsa/ais/MariwebSslConnectMain.java
@@ -1,5 +1,7 @@
 package au.gov.amsa.ais;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,9 +18,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.X509ExtendedTrustManager;
 
 import au.gov.amsa.util.nmea.NmeaUtil;
 
@@ -29,10 +32,18 @@ public final class MariwebSslConnectMain {
     public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException,
             UnknownHostException, IOException, InterruptedException {
 
+        String host = checkNotNull(System.getProperty("host"), "host cannot be null");
+        int port = Integer.parseInt(checkNotNull(System.getProperty("port"), "port cannot be null"));
+        String password = checkNotNull(System.getProperty("password"), "password cannot be null");
+        String username = checkNotNull(System.getProperty("username"), "username cannot be null");
+        if (password == null)
+            throw new IllegalArgumentException("you must set the 'password' system property");
+
         SSLContext context = setAlwaysHappyTrustManager(false);
         SSLSocketFactory factory = context.getSocketFactory();
         // Socket socket = factory.createSocket("mariweb.amsa.gov.au", 9005);
-        Socket socket = factory.createSocket("5.2.17.137", 9004);
+
+        Socket socket = factory.createSocket(host, port);
 
         CountDownLatch latch = new CountDownLatch(1);
         ExecutorService executor = Executors.newSingleThreadExecutor();
@@ -45,12 +56,7 @@ public final class MariwebSslConnectMain {
         OutputStream out = socket.getOutputStream();
         System.out.println("sending command");
 
-        String password = System.getProperty("password");
-        if (password == null)
-            throw new IllegalArgumentException("you must set the 'password' system property");
-
-        String command = "$PMWLSS," + System.currentTimeMillis() / 1000 + ",4,amsa," + password
-                + ",1*";
+        String command = "$PMWLSS," + System.currentTimeMillis() / 1000 + ",4,"+ username + "," + password + ",1*";
         String checksum = NmeaUtil.getChecksum(command);
         command = command + checksum + "\r\n";
 
@@ -88,22 +94,45 @@ public final class MariwebSslConnectMain {
         return sslContext;
     }
 
-    private static class AlwaysHappyTrustManager implements X509TrustManager {
+    private static class AlwaysHappyTrustManager extends X509ExtendedTrustManager {
         @Override
-        public void checkClientTrusted(X509Certificate[] chain, String authType)
-                throws CertificateException {
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
             // don't throw exception
         }
 
         @Override
-        public void checkServerTrusted(X509Certificate[] chain, String authType)
-                throws CertificateException {
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
             // don't throw exception
         }
 
         @Override
         public final X509Certificate[] getAcceptedIssuers() {
             return null;
+        }
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] arg0, String arg1, Socket arg2) throws CertificateException {
+            // don't throw exception
+        }
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] arg0, String arg1, SSLEngine arg2)
+                throws CertificateException {
+            // don't throw exception
+
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] arg0, String arg1, Socket arg2) throws CertificateException {
+            // don't throw exception
+
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] arg0, String arg1, SSLEngine arg2)
+                throws CertificateException {
+            // don't throw exception
+
         }
     }
 
