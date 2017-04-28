b8f836482c3f84fdb8b5441a4258864170c31404
Dave Moten <davidmoten@gmail.com>
2017-04-28 15:01:08 +1000

    update for extract

--- behaviour-detector/src/test/java/au/gov/amsa/geo/adhoc/ShipStaticDataMain.java
+++ behaviour-detector/src/test/java/au/gov/amsa/geo/adhoc/ShipStaticDataMain.java
@@ -11,14 +11,21 @@ import au.gov.amsa.util.Files;
 public class ShipStaticDataMain {
 
     public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("started");
+        long t = System.currentTimeMillis();
         File outputFile = new File("target/out.txt");
         // String filename = "/media/an/nmea/2014/NMEA_ITU_20140815.gz";
         List<File> files = Files.find(new File("/media/an/nmea/2014/"),
                 Pattern.compile("NMEA_ITU.*.gz"));
+        files.addAll(Files.find(new File("/media/an/nmea/2015/"),
+                Pattern.compile("NMEA_ITU.*.gz")));
+        files.addAll(Files.find(new File("/media/an/nmea/2016/"),
+                Pattern.compile("NMEA_ITU.*.gz")));
 
         ShipStaticDataCreator.writeStaticDataToFile(files, outputFile).count().toBlocking()
                 .single();
-
+        t = System.currentTimeMillis() - t;
+        System.out.println("finished in "+ t/1000 + "s");
     }
 
 }
