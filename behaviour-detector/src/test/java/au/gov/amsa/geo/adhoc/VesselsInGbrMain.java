f8240f4cbe8fb5077af1b678c7de574ff8312ae1
Dave Moten <davidmoten@gmail.com>
2017-05-25 15:02:26 +1000

    add VesselsInGbrMain

--- behaviour-detector/src/test/java/au/gov/amsa/geo/adhoc/VesselsInGbrMain.java
+++ behaviour-detector/src/test/java/au/gov/amsa/geo/adhoc/VesselsInGbrMain.java
@@ -1,6 +1,11 @@
 package au.gov.amsa.geo.adhoc;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -10,26 +15,50 @@ import au.gov.amsa.risky.format.BinaryFixes;
 import au.gov.amsa.risky.format.Fix;
 import au.gov.amsa.util.Files;
 import rx.Observable;
+import rx.Scheduler;
+import rx.observables.GroupedObservable;
+import rx.schedulers.Schedulers;
 
 public class VesselsInGbrMain {
 
-    public static void main(String[] args) {
-        Pattern pattern = Pattern.compile(".*\\.track");
-        List<File> list = new ArrayList<File>();
-        list.addAll(Files.find(new File("/media/an/binary-fixes-5-minute/2014"), pattern));
-        list.addAll(Files.find(new File("/media/an/binary-fixes-5-minute/2015"), pattern));
-        list.addAll(Files.find(new File("/media/an/binary-fixes-5-minute/2016"), pattern));
-        AtomicInteger count = new AtomicInteger();
-        Observable.from(list) //
-                .groupBy(f -> count.getAndIncrement() % Runtime.getRuntime().availableProcessors()) //
-                .flatMap(files -> files.doOnNext(System.out::println) //
-                        .flatMap(f -> BinaryFixes.from(f) //
-                                .filter(fix -> inGbr(fix)) //
-                                .map(fix -> fix.mmsi()) //
-                                .distinct()))
-                .distinct() //
-                .toBlocking() //
-                .forEach(System.out::println);
+    public static void main(String[] args) throws IOException {
+        File out = new File("target/mmsi.txt");
+        out.delete();
+        try (BufferedWriter b = new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream(out)))) {
+            Pattern pattern = Pattern.compile(".*\\.track");
+            List<File> list = new ArrayList<File>();
+            list.addAll(Files.find(new File("/media/an/binary-fixes-5-minute/2014"), pattern));
+            list.addAll(Files.find(new File("/media/an/binary-fixes-5-minute/2015"), pattern));
+            list.addAll(Files.find(new File("/media/an/binary-fixes-5-minute/2016"), pattern));
+            AtomicInteger count = new AtomicInteger();
+            Observable.from(list) //
+                    .groupBy(f -> count.getAndIncrement()
+                            % Runtime.getRuntime().availableProcessors()) //
+                    .flatMap(files -> vesselsInGbr(files, Schedulers.computation())).distinct() //
+                    .sorted() //
+                    .doOnNext(m -> write(b, m)) //
+                    .toBlocking() //
+                    .subscribe();
+        }
+    }
+
+    private static void write(BufferedWriter b, Integer m) {
+        try {
+            b.write(String.valueOf(m));
+            b.write('\n');
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Observable<Integer> vesselsInGbr(GroupedObservable<Integer, File> files,
+            Scheduler scheduler) {
+        return files //
+                .flatMap(f -> BinaryFixes.from(f) //
+                        .filter(fix -> inGbr(fix)) //
+                        .map(fix -> fix.mmsi()) //
+                        .distinct().subscribeOn(scheduler));
     }
 
     private static boolean inGbr(Fix fix) {
