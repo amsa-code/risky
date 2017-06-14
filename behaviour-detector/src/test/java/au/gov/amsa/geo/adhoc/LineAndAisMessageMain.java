9cc4250d5e203384fe4fabc058603572a5ce7126
Dave Moten <davidmoten@gmail.com>
2017-06-14 17:51:44 +1000

    adhoc dev

--- behaviour-detector/src/test/java/au/gov/amsa/geo/adhoc/LineAndAisMessageMain.java
+++ behaviour-detector/src/test/java/au/gov/amsa/geo/adhoc/LineAndAisMessageMain.java
@@ -1,23 +1,29 @@
 package au.gov.amsa.geo.adhoc;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashSet;
 import java.util.Set;
 
 import au.gov.amsa.ais.rx.Streams;
 
 public class LineAndAisMessageMain {
-    
+
     private static final String TAG_BLOCK_REGEX = "\\\\.*\\*..?\\\\";
 
     public static void main(String[] args) throws IOException {
+        long t = System.currentTimeMillis();
         String a = "\\s:Sugarloaf Point,c:1495756800,T:2017-05-26 00.00.00*64\\!BSVDM,1,1,,B,15DQAR00imbrvf?eEIjp<Faj00S1,0*65";
         System.out.println(a.replaceFirst("\\\\.*\\*..\\\\", ""));
-//        System.exit(0);
+        BufferedWriter b = new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream("target/out.txt")));
+        // System.exit(0);
         Set<String> set = new HashSet<>(7000000);
         for (int i = 1; i <= 10; i++) {
             File f = new File(
@@ -28,24 +34,39 @@ public class LineAndAisMessageMain {
                     new InputStreamReader(new FileInputStream(f)))) {
                 String line;
                 while ((line = br.readLine()) != null) {
+                    b.write(line.trim());
+                    b.write("\n");
                     String s = line.trim().replaceFirst(TAG_BLOCK_REGEX, "");
                     set.add(s);
                 }
             }
         }
+        b.close();
+        Set<String> set2 = new HashSet<String>();
+        System.out.println("written");
         Streams.nmeaFromGzip(new File("/media/an/amsa_26_05_2017_5_IEC/iec/2017-05-26.txt.gz")) //
                 .compose(o -> Streams.extract(o)) //
                 .filter(m -> m.getMessage().isPresent()
                         && is123(m.getMessage().get().message().getMessageId())) //
-                .filter(x -> x.getMessage().get().time() >= 1495756800000L+ 100000) //
+                .filter(x -> x.getMessage().get().time() >= 1495756800000L + 100000) //
                 .map(x -> x.getLine().trim()) //
-                .filter(x -> !x.startsWith("\\1G2:") && ! x.startsWith("\\1G3") && !x.startsWith("\\2G3"))
+                .filter(x -> !x.startsWith("\\1G2:") && !x.startsWith("\\1G3")
+                        && !x.startsWith("\\2G3"))
                 .filter(x -> !set.contains(x.replaceFirst(TAG_BLOCK_REGEX, ""))) //
-                .doOnNext(System.out::println) //
+                .doOnNext(x -> set2.add(x.replaceFirst(TAG_BLOCK_REGEX, ""))) //
+//                .doOnNext(System.out::println) //
                 .count() //
                 .doOnNext(System.out::println) //
                 .toBlocking() //
                 .subscribe();
+        System.out.println((System.currentTimeMillis()-t)+ "ms");
+        
+        for (String s:set) {
+            if (!set2.contains(s)) {
+                System.out.println(s);
+            }
+        }
+        System.out.println((System.currentTimeMillis()-t)+ "ms");
     }
 
     private static boolean is123(int n) {
