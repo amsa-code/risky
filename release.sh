c6eaf4591a01b52e7b38da5b5d877c6716af2935
Dave Moten <davidmoten@gmail.com>
2017-11-02 15:39:14 +1100

    update internal maven repo

--- release.sh
+++ release.sh
@@ -15,7 +15,7 @@ git push
 git tag -a $VERSION -m "$VERSION"
 git push origin $VERSION
 git checkout $VERSION
-mvn deploy -Dmode=test -Drisky.repo=amsa-maven -Drisky.repo.url=http://maven.amsa.gov.au:8081/artifactory/libs-releases-local
+mvn deploy -Dmode=test -Drisky.repo=amsa-maven -Drisky.repo.url=http://mvlrep001.infra.amsa.gov.au:8081/artifactory/libs-releases-local
 git checkout master
 mvn versions:set -DnewVersion=$VERSION.1-SNAPSHOT -DgenerateBackupPoms=false
 git commit -am "setting versions to snapshot"
