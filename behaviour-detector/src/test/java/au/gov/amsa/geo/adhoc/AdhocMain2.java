538ae6e6d7568dd11079190a0fb0565060e81cd2
Dave Moten <davidmoten@gmail.com>
2017-11-22 11:44:41 +1100

    hilbert-curve dev

--- behaviour-detector/src/test/java/au/gov/amsa/geo/adhoc/AdhocMain2.java
+++ behaviour-detector/src/test/java/au/gov/amsa/geo/adhoc/AdhocMain2.java
@@ -28,8 +28,8 @@ public class AdhocMain2 {
 
     public static void main(String[] args) throws IOException {
 
-        String folder = "/home/dave/Downloads";
-        // String folder = "/media/an/temp";
+        // String folder = "/home/dave/Downloads";
+        String folder = "/media/an/temp";
         File file = new File(folder + "/2017-11-16.fix");
 
         int choice = 2;
@@ -52,26 +52,30 @@ public class AdhocMain2 {
 
             long maxTime = BinaryFixes.from(file, false, BinaryFixesFormat.WITH_MMSI) //
                     .map(x -> x.time()).reduce((x, y) -> Math.max(x, y)).toBlocking().single();
-            System.out.println("start=" + new Date(minTime) + ", finish=" + new Date(maxTime));
+            System.out.println("start=" + new Date(minTime) + ", finish=" + new Date(maxTime) + ", [" + minTime + ","
+                    + maxTime + "]");
 
-            int bits = 20;
+            int bits = 10;
             int dimensions = 3;
             SmallHilbertCurve h = HilbertCurve.small().bits(bits).dimensions(dimensions);
             long maxIndexes = 1L << bits * dimensions;
             long maxOrdinates = 1L << bits;
 
             // get ranges for Sydney query to measure effectiveness
-            double lat1 = -33.806477;
-            double lon1 = 151.181767;
+            float lat1 = -33.806477f;
+            float lon1 = 151.181767f;
             long t1 = minTime + (maxTime - minTime) / 2;
-            double lat2 = -33.882896;
-            double lon2 = 151.281330;
+            float lat2 = -33.882896f;
+            float lon2 = 151.281330f;
             long t2 = t1 + TimeUnit.HOURS.toMillis(1);
-            int splits = 1;
-            List<Range> ranges = h.query2(
-                    scalePoint(lat1, lon1, t1, minTime, maxTime, maxOrdinates),
+            int splits = 4;
+            System.out.println("calculating ranges");
+            long timer = System.currentTimeMillis();
+            List<Range> ranges = h.query2(scalePoint(lat1, lon1, t1, minTime, maxTime, maxOrdinates),
                     scalePoint(lat2, lon2, t2, minTime, maxTime, maxOrdinates), splits);
+            System.out.println("ranges calculated in " + (System.currentTimeMillis() - timer) + "ms");
             ranges.forEach(System.out::println);
+            System.out.println("numRanges=" + ranges.size());
 
             int numPartitions = 10;
             int[] counts = new int[numPartitions];
@@ -83,8 +87,8 @@ public class AdhocMain2 {
             BinaryFixes.from(file, false, BinaryFixesFormat.WITH_MMSI) //
                     .doOnNext(log(count)) //
                     .forEach(fix -> {
-                        long index = h.index(scalePoint(fix.lat(), fix.lon(), fix.time(), minTime,
-                                maxTime, maxOrdinates));
+                        long index = h
+                                .index(scalePoint(fix.lat(), fix.lon(), fix.time(), minTime, maxTime, maxOrdinates));
                         int partition = (int) (index / step);
                         counts[partition]++;
                         boolean inRange = false;
@@ -95,10 +99,9 @@ public class AdhocMain2 {
                                 break;
                             }
                         }
-                        if (fix.lat() >= Math.min(lat1, lat2) && fix.lat() <= Math.max(lat1, lat2)
-                                && fix.lon() >= Math.min(lon1, lon2)
-                                && fix.lon() <= Math.max(lon1, lon2) && fix.time() >= t1
-                                && fix.time() <= t2) {
+                        if (between(Math.min(lat1, lat2), fix.lat(), Math.max(lat1, lat2))
+                                && between(Math.min(lon1, lon2), fix.lon(), Math.max(lon1, lon2)) //
+                                && fix.time() >= t1 && fix.time() <= t2) {
                             inBounds.incrementAndGet();
                             if (!inRange) {
                                 missed.incrementAndGet();
@@ -106,9 +109,8 @@ public class AdhocMain2 {
                         }
                     });
 
-            System.out.println("ranges=" + ranges.size() + ", hit rate="
-                    + inBounds.get() / ((double) inRanges.get()) + ", missed=" + missed.get()
-                    + " of " + inBounds.get());
+            System.out.println("ranges=" + ranges.size() + ", hit rate=" + inBounds.get() / ((double) inRanges.get())
+                    + ", missed=" + missed.get() + " of " + inBounds.get());
             System.out.println();
 
             System.out.println("===============");
@@ -134,8 +136,7 @@ public class AdhocMain2 {
 
             OutputStream[] outs = new OutputStream[numPartitions];
             for (int i = 0; i < outs.length; i++) {
-                outs[i] = new BufferedOutputStream(
-                        new FileOutputStream(new File(folder + "/partition" + i + ".fix")));
+                outs[i] = new BufferedOutputStream(new FileOutputStream(new File(folder + "/partition" + i + ".fix")));
             }
             count.set(0);
             ByteBuffer bb = BinaryFixes.createFixByteBuffer(BinaryFixesFormat.WITH_MMSI);
@@ -144,8 +145,8 @@ public class AdhocMain2 {
                     .doOnNext(fix -> {
                         long x = Math.round(Math.floor((fix.lat() + 90) / 180.0 * maxIndexes));
                         long y = Math.round(Math.floor((fix.lon() + 180) / 360.0 * maxIndexes));
-                        long z = Math.round(Math.floor((fix.time() - minTime)
-                                / ((double) maxTime - minTime) * maxIndexes));
+                        long z = Math
+                                .round(Math.floor((fix.time() - minTime) / ((double) maxTime - minTime) * maxIndexes));
                         long index = h.index(x, y, z);
                         int partition = (int) (index / step);
                         bb.clear();
@@ -165,25 +166,27 @@ public class AdhocMain2 {
         }
     }
 
+    private static boolean between(float a, float b, float c) {
+        return a <= b && b <= c;
+    }
+
     private static Action1<? super Fix> log(AtomicLong count) {
         return f -> {
             long c = count.incrementAndGet();
             if (c % 1000000 == 0) {
-                System.out
-                        .println("count=" + new DecimalFormat("0.###").format(c / 1000000.0) + "m");
+                System.out.println("count=" + new DecimalFormat("0.###").format(c / 1000000.0) + "m");
             }
         };
     }
 
-    private static long[] scalePoint(double lat, double lon, long time, long minTime, long maxTime,
-            long max) {
-        long x = scale((lat + 90.0) / 180, max);
-        long y = scale((lon + 180.0) / 360, max);
-        long z = scale(((double) time - minTime) / (maxTime - minTime), max);
+    private static long[] scalePoint(float lat, float lon, long time, long minTime, long maxTime, long max) {
+        long x = scale((lat + 90.0f) / 180, max);
+        long y = scale((lon + 180.0f) / 360, max);
+        long z = scale(((float) time - minTime) / (maxTime - minTime), max);
         return new long[] { x, y, z };
     }
 
-    private static long scale(double d, long max) {
+    private static long scale(float d, long max) {
         Preconditions.checkArgument(d >= 0 && d <= 1);
         if (d == 1) {
             return max;
