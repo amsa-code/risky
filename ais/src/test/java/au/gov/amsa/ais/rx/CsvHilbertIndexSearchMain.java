8ff2d42478185379ae33c114041f902b0e4b64b9
David Moten <davidmoten@gmail.com>
2019-06-28 15:31:36 +1000

    use sparse-hilbert-index 0.1.4-SNAPSHOT, will break build

--- ais/src/test/java/au/gov/amsa/ais/rx/CsvHilbertIndexSearchMain.java
+++ ais/src/test/java/au/gov/amsa/ais/rx/CsvHilbertIndexSearchMain.java
@@ -28,45 +28,39 @@ public class CsvHilbertIndexSearchMain {
                 throw new RuntimeException(e);
             }
         };
-        Serializer<CSVRecord> serializer = Serializer.csv(
-                CSVFormat.DEFAULT.withRecordSeparator('\n'),
+        Serializer<CSVRecord> serializer = Serializer.csv(CSVFormat.DEFAULT.withRecordSeparator('\n'),
                 StandardCharsets.UTF_8);
-        Index<CSVRecord> index = Index
-                .serializer(serializer) //
+        Index<CSVRecord> index = Index.serializer(serializer) //
                 .pointMapper(pointMapper) //
-                .read(new File(System.getProperty("user.home")
-                        + "/Downloads/2018-11-27-positions-sorted.csv.idx"));
-        
+                .read(new File(System.getProperty("user.home") + "/Downloads/2018-11-27-positions-sorted.csv.idx"));
+
         long t1 = Math.round(index.mins()[2]) + TimeUnit.HOURS.toMillis(12);
         long t2 = t1 + TimeUnit.MINUTES.toMillis(60);
-        final Bounds sydney = Bounds.create(new double[] { -33.68, 150.86, t1 },
-                new double[] { -34.06, 151.34, t2 });
+        final Bounds sydney = Bounds.create(new double[] { -33.68, 150.86, t1 }, new double[] { -34.06, 151.34, t2 });
         final Bounds sydneyAllDay = Bounds.create(new double[] { -33.68, 150.86, index.mins()[2] },
                 new double[] { -34.06, 151.34, index.maxes()[2] });
-        final Bounds brisbane = Bounds.create(new double[] { -24.9, 150, t1 },
-                new double[] { -29.5, 158, t2 });
-        final Bounds qld = Bounds.create(new double[] { -9.481, 137.3, t1 },
-                new double[] { -29.0, 155.47, t2 });
-        final Bounds tas = Bounds.create(new double[] { -39.389, 143.491, t1 },
-                new double[] { -44, 149.5, t2 });
-        File sorted = new File(System.getProperty("user.home")
-                + "/Downloads/2018-11-27-positions-sorted.csv");
-        index.search(sydney).withStats()
-                .file(sorted)
-                .last().forEach(System.out::println);
+        final Bounds brisbane = Bounds.create(new double[] { -24.9, 150, t1 }, new double[] { -29.5, 158, t2 });
+        final Bounds qld = Bounds.create(new double[] { -9.481, 137.3, t1 }, new double[] { -29.0, 155.47, t2 });
+        final Bounds tas = Bounds.create(new double[] { -39.389, 143.491, t1 }, new double[] { -44, 149.5, t2 });
+        File sorted = new File(System.getProperty("user.home") + "/Downloads/2018-11-27-positions-sorted.csv");
+        index.search(sydney) //
+                .withStats() //
+                .file(sorted) //
+                .lastOrError() // 
+                .subscribe(System.out::println);
         for (Bounds bounds : new Bounds[] { sydney, sydneyAllDay, brisbane, qld, tas }) {
-            index.search(bounds).withStats().file(sorted)
-                    .last().forEach(System.out::println);
+            index.search(bounds).withStats().file(sorted).last().forEach(System.out::println);
         }
-        
+
         System.out.println("loading index from s3");
         long t = System.currentTimeMillis();
-        index = Index.serializer(serializer).pointMapper(pointMapper).read(new URL("https://moten-fixes.s3-ap-southeast-2.amazonaws.com/2018-11-27-positions-sorted.csv.idx"));
+        index = Index.serializer(serializer).pointMapper(pointMapper).read(
+                new URL("https://moten-fixes.s3-ap-southeast-2.amazonaws.com/2018-11-27-positions-sorted.csv.idx"));
         System.out.println("index loaded in " + (System.currentTimeMillis() - t) + "ms");
         System.out.println("searching via s3");
         for (Bounds bounds : new Bounds[] { sydney, sydneyAllDay, brisbane, qld, tas }) {
-            index.search(bounds).withStats().url(
-                    "https://moten-fixes.s3-ap-southeast-2.amazonaws.com/2018-11-27-positions-sorted.csv") //
+            index.search(bounds).withStats()
+                    .url("https://moten-fixes.s3-ap-southeast-2.amazonaws.com/2018-11-27-positions-sorted.csv") //
                     .last().forEach(System.out::println);
         }
     }
