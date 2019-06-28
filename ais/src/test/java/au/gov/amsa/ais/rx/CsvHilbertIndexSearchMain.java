4ca49cfbf6433feb47724389b818c90fc9bcbd78
Dave Moten <davidmoten@gmail.com>
2019-06-29 07:51:33 +1000

    csv search, just s3

--- ais/src/test/java/au/gov/amsa/ais/rx/CsvHilbertIndexSearchMain.java
+++ ais/src/test/java/au/gov/amsa/ais/rx/CsvHilbertIndexSearchMain.java
@@ -1,6 +1,5 @@
 package au.gov.amsa.ais.rx;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -31,10 +30,16 @@ public class CsvHilbertIndexSearchMain {
         };
         Serializer<CSVRecord> serializer = Serializer.csv(CSVFormat.DEFAULT.withRecordSeparator('\n'),
                 StandardCharsets.UTF_8);
-        Index<CSVRecord> index = Index.serializer(serializer) //
-                .pointMapper(pointMapper) //
-                .read(new File(System.getProperty("user.home") + "/Downloads/2018-11-27-positions-sorted.csv.idx"));
+//        Index<CSVRecord> index = Index.serializer(serializer) //
+//                .pointMapper(pointMapper) //
+//                .read(new File(System.getProperty("user.home") + "/Downloads/2018-11-27-positions-sorted.csv.idx"));
 
+        System.out.println("loading index from s3");
+        long t = System.currentTimeMillis();
+        Index<CSVRecord> index = Index.serializer(serializer).pointMapper(pointMapper).read(
+                new URL("https://moten-fixes.s3-ap-southeast-2.amazonaws.com/2018-11-27-positions-sorted.csv.idx"));
+        System.out.println("index loaded in " + (System.currentTimeMillis() - t) + "ms");
+        
         long t1 = Math.round(index.mins()[2]) + TimeUnit.HOURS.toMillis(12);
         long t2 = t1 + TimeUnit.MINUTES.toMillis(60);
         final Bounds sydney = Bounds.create(new double[] { -33.68, 150.86, t1 }, new double[] { -34.06, 151.34, t2 });
@@ -43,21 +48,16 @@ public class CsvHilbertIndexSearchMain {
         final Bounds brisbane = Bounds.create(new double[] { -24.9, 150, t1 }, new double[] { -29.5, 158, t2 });
         final Bounds qld = Bounds.create(new double[] { -9.481, 137.3, t1 }, new double[] { -29.0, 155.47, t2 });
         final Bounds tas = Bounds.create(new double[] { -39.389, 143.491, t1 }, new double[] { -44, 149.5, t2 });
-        File sorted = new File(System.getProperty("user.home") + "/Downloads/2018-11-27-positions-sorted.csv");
-        index.search(sydney) //
-                .withStats() //
-                .file(sorted) //
-                .lastOrError() //
-                .subscribe(System.out::println);
-        for (Bounds bounds : new Bounds[] { sydney, sydneyAllDay, brisbane, qld, tas }) {
-            index.search(bounds).withStats().file(sorted).lastOrError().doOnSuccess(System.out::println).blockingGet();
-        }
+//        File sorted = new File(System.getProperty("user.home") + "/Downloads/2018-11-27-positions-sorted.csv");
+//        index.search(sydney) //
+//                .withStats() //
+//                .file(sorted) //
+//                .lastOrError() //
+//                .subscribe(System.out::println);
+//        for (Bounds bounds : new Bounds[] { sydney, sydneyAllDay, brisbane, qld, tas }) {
+//            index.search(bounds).withStats().file(sorted).lastOrError().doOnSuccess(System.out::println).blockingGet();
+//        }
 
-        System.out.println("loading index from s3");
-        long t = System.currentTimeMillis();
-        index = Index.serializer(serializer).pointMapper(pointMapper).read(
-                new URL("https://moten-fixes.s3-ap-southeast-2.amazonaws.com/2018-11-27-positions-sorted.csv.idx"));
-        System.out.println("index loaded in " + (System.currentTimeMillis() - t) + "ms");
         System.out.println("searching via s3");
 
         for (Bounds bounds : new Bounds[] { sydney, sydneyAllDay, brisbane, qld, tas }) {
