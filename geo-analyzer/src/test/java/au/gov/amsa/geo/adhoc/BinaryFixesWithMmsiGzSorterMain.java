01ecbc4850acfb40aceeb8a09d9c323f68d95791
David Moten <davidmoten@gmail.com>
2020-08-26 16:37:32 +1000

    add logging

--- geo-analyzer/src/test/java/au/gov/amsa/geo/adhoc/BinaryFixesWithMmsiGzSorterMain.java
+++ geo-analyzer/src/test/java/au/gov/amsa/geo/adhoc/BinaryFixesWithMmsiGzSorterMain.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UncheckedIOException;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.zip.GZIPOutputStream;
 
 import org.apache.log4j.Logger;
@@ -13,6 +14,7 @@ import org.apache.log4j.Logger;
 import au.gov.amsa.risky.format.BinaryFixes;
 import au.gov.amsa.risky.format.BinaryFixesFormat;
 import rx.Observable;
+import rx.schedulers.Schedulers;
 
 public class BinaryFixesWithMmsiGzSorterMain {
 
@@ -23,19 +25,17 @@ public class BinaryFixesWithMmsiGzSorterMain {
         File tracksDir = new File("/home/dxm/AIS/tracks");
         File sortedTracks = new File("/home/dxm/AIS/tracks-sorted");
 
-        BinaryFixes.from(new File(tracksDir, "2019-01-01.track.gz"), true, BinaryFixesFormat.WITH_MMSI) //
-                .doOnNext(System.out::println) //
-                .subscribe();
-
         sortedTracks.mkdir();
         Observable<File> files = Observable //
                 .from(tracksDir.listFiles()) //
                 .filter(x -> x.getName().endsWith(".track.gz"));
+        AtomicInteger n = new AtomicInteger();
         files.flatMap(x -> Observable //
                 .just(x) //
-                .doOnNext(f -> log.info("sorting " + f)) //
+                .observeOn(Schedulers.computation()) //
+                .doOnNext(f -> log.info(n.incrementAndGet() + ": sorting " + f.getName())) //
                 .doOnNext(f -> sort(f, sortedTracks))) //
-                .doOnNext(f -> log.info("sorted " + f)) //
+                .doOnNext(f -> log.info(n.get() + ": sorted " + f.getName())) //
                 .toBlocking() //
                 .subscribe();
     }
@@ -44,7 +44,6 @@ public class BinaryFixesWithMmsiGzSorterMain {
         File sorted = new File(outputDir, file.getName());
         try (OutputStream out = new GZIPOutputStream(new BufferedOutputStream(new FileOutputStream(sorted)))) {
             BinaryFixes.from(file, true, BinaryFixesFormat.WITH_MMSI) //
-                    .doOnNext(System.out::println) //
                     .toSortedList((x, y) -> Long.compare(x.time(), y.time())) //
                     .flatMap(x -> Observable.from(x)) //
                     .doOnNext(fix -> BinaryFixes.write(fix, out, BinaryFixesFormat.WITH_MMSI)) //
