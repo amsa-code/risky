12868b31d323676f9f86c2dc04e2a238214b7e68
Dave Moten <davidmoten@gmail.com>
2022-03-16 09:38:15 +1100

    dev

--- geo-analyzer/src/main/java/au/gov/amsa/geo/CsvSplitterByYear.java
+++ geo-analyzer/src/main/java/au/gov/amsa/geo/CsvSplitterByYear.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.zip.GZIPOutputStream;
 
@@ -17,12 +18,13 @@ public class CsvSplitterByYear {
     public void split() throws IOException {
         System.out.println("starting split by year");
         // expects id,yyyy-MM-dd etc
-        File input = new File("/home/dave/export.txt");
+        File input = new File(System.getProperty("user.home") + "/export.txt");
         File outputDirectory = new File("target/yearly");
         outputDirectory.mkdirs();
         try {
             long count = 0;
-            Iterator<String> it = Files.lines(input.toPath()).iterator(); //
+            long t = System.currentTimeMillis();
+            Iterator<String> it = Files.lines(input.toPath()).skip(1).iterator(); //
             while (it.hasNext()) {
                 String line = it.next();
                 count += 1;
@@ -38,8 +40,16 @@ public class CsvSplitterByYear {
                 // delayed are ignored (some satellite reports)
                 if (currentYear == null || year.compareTo(currentYear) >= 0) {
                     if (!year.equals(currentYear)) {
-                        System.out.println("starting year " + year);
+                        if (out != null) {
+                            out.close();
+                            long t2 = System.currentTimeMillis();
+                            double minutes = (t2 - t)/ 60000.0;
+                            t = t2;
+                            System.out.println("completed year " + year + " in " + minutes + " minutes");
+                        }
+                        System.out.println("starting year " + year + " " + new Date());
                         currentYear = year;
+                        
                         try {
                             out = new PrintStream(new GZIPOutputStream(
                                     new FileOutputStream(new File(outputDirectory, year + ".txt.gz"))));
