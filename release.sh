9d1f4267e67617f3d9027e827947287c023ea28d
Olivier Lamy <olamy@apache.org>
2024-03-20 15:42:47 +1000

    fix release.sh
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- release.sh
+++ release.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
-set -e 
-if [ $# -ne 1 ]
-then
-  echo "Usage: ./`basename $0` <VERSION>"
-  # `basename $0` is the script's filename.
-  exit 1 
-fi
-VERSION=$1
-git pull origin master
-mvn versions:set -DnewVersion=$VERSION -DgenerateBackupPoms=false
-## test build works
-mvn clean install
-git commit -am "prepare for release $VERSION"
-git tag -a $VERSION -m "$VERSION"
-git push origin $VERSION
-git push origin master
-mvn versions:set -DnextSnapshot=true -DgenerateBackupPoms=false
-git commit -am "set versions to next snapshot"
-git push origin master
+mvn release:prepare release:perform -DskipTests -Dmaven.release.plugin.arguments="-DskipTests -Dspotbugs.skip=true" -DlocalCheckout=true -B
+
 echo =========================================================================
-echo At this point the tag has been pushed to the git repository on GitHub
-echo and you should go to the GitHub UI to indicate that that tag is the 
-echo latest release which will trigger the workflow that pushes the versioned
-echo binaries to Maven Central.
+echo At this point you need to log on to https://oss.sonatype.org/
 echo =========================================================================
  
